de82b3a0716b808c4432c9dc21b588bbc9e3d719
ricardo <ricardo.martinez@bq.com>
2015-08-10 09:34:59 +0200

    fail routes
    Change-Id: I717f62066a0f3a254d72ace804938382519813b8

---
+++ src/common/resources/resourcesCommon.js
@@ -0,0 +1,67 @@
+(function() {
+    //@exclude
+    'use strict';
+    /*globals corbel */
+    //@endexclude
+
+    corbelTest.resources = {};
+    var createdQueryObject = [];
+
+    var successHandler = function(list, collection, id) {
+        list.push({
+            collection: collection,
+            id: id
+        });
+    };
+
+    var createIntegerSecuence = function(length) {
+        var list = [];
+        for (var i = 1; i <= length; i++) {
+            list.push(i);
+        }
+        return list;
+    };
+
+    corbelTest.resources.createdObjectsToQuery = function(driver, collectionName, amount, extraField) {
+        var promises = [];
+
+        for (var count = 1; count <= amount; count++) {
+            var json = {
+                stringField: 'stringFieldContent' + count,
+                intField: 100 * count,
+                computableField: count + (1/3),
+                stringSortCut: 'Test Short Cut',
+                codingTest: 'ñÑçáéíóúàèìòùâêîôû\'',
+                ObjectNumber: createIntegerSecuence(count),
+                ObjectMatch: [{
+                    name: 'basic',
+                    identifier: 'id' + count,
+                    type: 'basic'
+                }, {
+                    name: 'premium',
+                    identifier: 'id' + count,
+                    type: 'premium'
+                }]
+            };
+
+            if(extraField) {
+                json.extra = extraField; 
+            }
+
+            var promise = driver.resources.collection(collectionName).add(json);
+            promises.push(promise);
+            promise.then(successHandler.bind(this, createdQueryObject, collectionName));
+        }
+        return Promise.all(promises);
+    };
+
+    corbelTest.resources.cleanResourcesQuery = function(driver) {
+        var promises = [];
+        createdQueryObject.forEach(function(entry) {
+            var promise = driver.resources.resource(entry.collection, entry.id).delete();
+            promises.push(promise);
+        });
+        createdQueryObject = [];
+        return Promise.all(promises);
+    };
+})();
