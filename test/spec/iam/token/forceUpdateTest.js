5b15b1de6a15caddbb9f646b1c74768c1e16db33
laurasada <laura.sada@bq.com>
2015-08-12 12:29:36 +0200

    IAM token tests migrated
    Change-Id: I8a0355ddefd98a3691407b5cdbe8c34b29339b66

---
+++ test/spec/iam/token/forceUpdateTest.js
@@ -0,0 +1,55 @@
+    describe('In IAM module, when I try to make a token request', function() {
+
+      var corbelDriver;
+      var aud = _.cloneDeep(corbelTest.CONFIG['aud']);
+      var claimDefault = _.cloneDeep(corbelTest.CONFIG['DEFAULT_CLIENT']);
+      var claims = {
+        iss: claimDefault.clientId,
+        aud: aud,
+        scope: claimDefault.scopes
+      };
+
+      before(function() {
+        corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'];
+      });
+
+      var UNSUPPORTED_VERSION = '0.0.0';
+      var SUPPORTED_VERSION = '1.0.0';
+      var FORCE_UPDATE_MESSAGE = 'unsupported_version';
+      var FORCE_UPDATE_CODE = 403;
+
+      it('without version, it works', function(done) {
+        claims.version = undefined;
+        corbelDriver.iam.token().create({
+          jwt: corbel.jwt.generate(claims, claimDefault.clientSecret)
+        }).
+        should.notify(done);
+      });
+
+      it('with unsupported version version, it fails with 403 - "unsupported_version"', function(done) {
+
+        claims.version = UNSUPPORTED_VERSION;
+
+        corbelDriver.iam.token().create({
+          jwt: corbel.jwt.generate(claims, claimDefault.clientSecret)
+        }).
+        should.eventually.be.rejected.
+        then(function(e) {
+          //TODO
+          var error = JSON.parse(e.data.responseText);
+          expect(error.error).to.be.equal(FORCE_UPDATE_MESSAGE);
+          expect(e.status).to.be.equal(FORCE_UPDATE_CODE);
+        }).
+        should.notify(done);
+      });
+
+      it('with compatible version, it works', function(done) {
+
+        claims.version = SUPPORTED_VERSION;
+
+        corbelDriver.iam.token().create({
+          jwt: corbel.jwt.generate(claims, claimDefault.clientSecret)
+        }).
+        should.notify(done);
+      });
+    });
