75ab4239b316af40d969ca3bf3650887dd72bff5
laurasada <laura.sada@bq.com>
2015-08-21 13:23:37 +0200

    IAM module token oauth error
    Change-Id: I5cfb81aceb9e3eba5fe4526b9058b9fab9341857

---
+++ test/spec/iam/token/tokenOauthErrorsTest.js
@@ -0,0 +1,108 @@
+describe('In IAM module, When try to get token with', function() {
+
+    var testDriver = corbel.getDriver({
+        urlBase: corbelTest.CONFIG.COMMON.urlBase.replace('{{ENV}}', 'int')
+    });
+
+    var jwtAlgorithm = 'HS256';
+    var aud = 'http://iam.bqws.io';
+    var claimDefault = _.cloneDeep(corbelTest.CONFIG['DEFAULT_CLIENT']);
+    var socialOauths = ['facebook', 'google', 'silkroad'];
+    var domain = 'silkroad-qa';
+    var url1 = window.location.host.split(':')[0];
+    var url2 = (parseInt(window.location.host.split(':')[1]) + 1);
+    var requestInfoEndpoint = 'http://' + url1 + ':' + url2 + '/requestinfo';
+    var version = _.cloneDeep(corbelTest.CONFIG['VERSION']);
+
+    socialOauths.forEach(function(socialOauth) {
+        it('bad token in ' + socialOauth, function(done) {
+            var accessToken = 'badToken';
+
+            var claims = {
+                'iss': claimDefault.clientId,
+                'request_domain': domain,
+                'scope': claimDefault.scopes,
+                'aud': aud,
+                'version': version,
+                'oauth.service': socialOauth,
+                'oauth.access_token': accessToken
+            };
+
+            testDriver.iam.token()
+                .create({
+                    jwt: corbel.jwt.generate(
+                        claims,
+                        claimDefault.clientSecret,
+                        jwtAlgorithm
+                    )
+                })
+                .should.eventually.be.rejected
+                .then(function(response) {
+                    var error = response.data;
+                    expect(response).to.have.property('status', 401);
+                    expect(error).to.have.property('error', 'unauthorized');
+                })
+                .should.eventually.be.fulfilled.notify(done);
+        });
+
+        it('bad code in ' + socialOauth, function(done) {
+            var code = 'badCode';
+
+            var claims = {
+                'iss': claimDefault.clientId,
+                'request_domain': domain,
+                'scope': claimDefault.scopes,
+                'aud': aud,
+                'version': version,
+                'oauth.code': code,
+                'oauth.service': socialOauth,
+                'oauth.redirect_uri': requestInfoEndpoint
+            };
+
+            testDriver.iam.token()
+                .create({
+                    jwt: corbel.jwt.generate(
+                        claims,
+                        claimDefault.clientSecret,
+                        jwtAlgorithm
+                    )
+                })
+                .should.eventually.be.rejected
+                .then(function(response) {
+                    var error = response.data;
+                    expect(response).to.have.property('status', 401);
+                    expect(error).to.have.property('error', 'unauthorized');
+                })
+                .should.eventually.be.fulfilled.notify(done);
+        });
+
+        it('missing oauth params in ' + socialOauth, function(done) {
+
+            var claims = {
+                'iss': claimDefault.clientId,
+                'request_domain': domain,
+                'scope': claimDefault.scopes,
+                'aud': aud,
+                'version': version,
+                'oauth.service': socialOauth,
+                'oauth.redirect_uri': requestInfoEndpoint
+            };
+
+            testDriver.iam.token()
+                .create({
+                    jwt: corbel.jwt.generate(
+                        claims,
+                        claimDefault.clientSecret,
+                        jwtAlgorithm
+                    )
+                })
+                .should.eventually.be.rejected
+                .then(function(response) {
+                    var error = response.data;
+                    expect(response).to.have.property('status', 400);
+                    expect(error).to.have.property('error', 'missing_oauth_params');
+                })
+                .should.eventually.be.fulfilled.notify(done);
+        });
+    });
+});
