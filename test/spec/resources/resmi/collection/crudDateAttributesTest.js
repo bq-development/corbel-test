199d8105bfb087ac889e213a1d28299a1a147925
vipereybq <victor.perezrey@bq.com>
2015-08-25 14:04:12 +0200

    [Patch 2] CrudDateAttributesTest bugfix
    Change-Id: I464bc0e90804178b0ba680eb73ddbf5f1e6cd7ef

--- test/spec/resources/resmi/collection/crudDateAttributesTest.js
+++ test/spec/resources/resmi/collection/crudDateAttributesTest.js
@@ -1,6 +1,6 @@
 describe('In RESOURCES module', function() {
 
-    describe('In RESMI module, testing Crud ', function() {
+    describe('In RESMI module, testing Crud', function() {
         var corbelDriver;
 
         before(function() {
@@ -75,15 +75,42 @@ describe('In RESOURCES module', function() {
             });
         });
 
-        describe('when you create object with put method then _updatedAt equals _createdAt', function() {
+        describe('When you create an object with PUT method ', function() {
             var idRandom = 'id' + Date.now();
+            var resourceId;
+            var updatedAt;
 
             var TEST_OBJECT_CREATE_PUT = {
                 test: 'test',
                 test2: 'test2'
             };
 
-            it('and successes', function(done) {
+
+            it('_updatedAt and _createdAt fields are equal', function(done) {
+
+                corbelDriver.resources.resource(COLLECTION, idRandom)
+                .update(TEST_OBJECT_CREATE_PUT)
+                .should.eventually.be.fulfilled
+                .then(function() {
+                    return corbelDriver.resources.resource(COLLECTION, idRandom)
+                    .get()
+                    .should.eventually.be.fulfilled;
+                })
+                .then(function(response) {
+                    updatedAt = response.data._updatedAt;
+                    expect(response.data._updatedAt).to.be.equal(response.data._createdAt);
+                    expect(response.data.test).to.be.equal('test');
+                    expect(response.data.test2).to.be.equal('test2');
+                }).
+                should.eventually.be.fulfilled.notify(done);
+            });
+
+            it('if the resource is updated after its creation, _updatedAt gets modified', function(done) {
+
+                var TEST_OBJECT_CREATE_UPDATE = _.cloneDeep(TEST_OBJECT_CREATE_PUT);
+                TEST_OBJECT_CREATE_UPDATE.newField = 'newField';
+                TEST_OBJECT_CREATE_UPDATE.test = null;
+                TEST_OBJECT_CREATE_UPDATE.test2 = 'test2Updated';
 
                 corbelDriver.resources.resource(COLLECTION, idRandom)
                 .update(TEST_OBJECT_CREATE_PUT)
@@ -97,26 +124,21 @@ describe('In RESOURCES module', function() {
                     expect(response.data._updatedAt).to.be.equal(response.data._createdAt);
                     expect(response.data.test).to.be.equal('test');
                     expect(response.data.test2).to.be.equal('test2');
-                }).
-                should.eventually.be.fulfilled.notify(done);
-            });
 
-            it('and successes update', function(done) {
-                var testObject = _.cloneDeep(TEST_OBJECT);
-
-                testObject.newField = 'newField';
-                testObject.test2 = null;
-
-                return corbelDriver.resources.resource(COLLECTION, resourceId)
-                .update(testObject)
-                .should.eventually.be.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.resource(COLLECTION, resourceId)
+                    return corbelDriver.resources.resource(COLLECTION, idRandom)
+                    .update(TEST_OBJECT_CREATE_UPDATE)
+                    .should.eventually.be.fulfilled;
+                }).then(function(response) {
+                    return corbelDriver.resources.resource(COLLECTION, idRandom)
                     .get()
                     .should.eventually.be.fulfilled;
                 })
                 .then(function(response) {
                     expect(response.data._updatedAt).to.be.greaterThan(updatedAt);
+                    expect(response.data.newField).to.be.equal('newField');
+                    expect(response.data.test).to.be.equal(undefined);
+                    expect(response.data.test2).to.be.equal('test2Updated');
+                    return response;
                 })
                 .should.eventually.be.fulfilled.notify(done);
             });
