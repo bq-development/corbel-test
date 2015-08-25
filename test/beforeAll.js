c6b521f3ad8587b52b3a0feb8f96cea8f8c08108
samuelmartingc <samuelmartingc@gmail.com>
2015-08-25 11:29:08 +0200

    added proxyLinkTest(skipped)
    Change-Id: I239419df778e4e130e122fc3db89fbba1aa495ed

--- test/beforeAll.js
+++ test/beforeAll.js
@@ -2,7 +2,7 @@ var sandbox = sinon.sandbox.create();
 
 before(function(done) {
 
-  this.timeout(10000);
+  this.timeout(30000);
 
   corbelTest.common.clients.loginAll().then(function() {
     done();
