2a214ed0d5d3a974417f39830bcaeabcee3a895a
ricardo <ricardo.martinez@bq.com>
2015-08-26 08:34:44 +0200

    Refactor in collectionAggregationTest and collectionConditionTest
    Change-Id: I92c5d4070ceb6ac104becf31635c1e91c15d3a00

--- test/spec/resources/resmi/collection/aggregation/collectionAggregationTest.js
+++ test/spec/resources/resmi/collection/aggregation/collectionAggregationTest.js
@@ -5,190 +5,177 @@ describe('In RESOURCES module', function() {
       corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'];
     });
 
-    describe('In RESMI module, testing aggregation,', function() {
-        this.timeout(10000);
-        
-        var COLLECTION = 'test:CorbelJSObjectAggregation' + Date.now();
-
+    describe('In RESMI module, testing collection aggregation,', function() {
+        var COLLECTION = 'test:CorbelJSObjectCollectionAggregation' + Date.now();
         var amount = 50;
         var sum = 25 * 155 / 3;
 
-        before(function(done) {
-
+        beforeEach(function(done) {
             corbelTest.common.resources.createdObjectsToQuery(corbelDriver, COLLECTION, amount)
-            .should.eventually.be.fulfilled.notify(done);
+            .should.be.eventually.fulfilled.and.notify(done);
         });
 
-        after(function(done) {
+        afterEach(function(done) {
             corbelTest.common.resources.cleanResourcesQuery(corbelDriver)
             .should.eventually.be.fulfilled
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('we get the collection with count aggregation and' +
+               ' returns the number of elements in the collection', function(done) {
+            var params = {
+                aggregation: {
+                    '$count': '*'
+                }
+            };
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('count').to.be.equal(amount);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('we get the collection with count aggregation and a query' +
+               ' and returns the number of elements that match the query', function(done) {
+            var params = {
+                aggregation: {
+                    '$count': '*'
+                },
+                query: [{
+                    '$eq': {
+                        intField: 1000
+                    }
+                }]
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('count').to.be.equal(1);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('we get the collection with sum aggregation and returns the expected sum', function(done) {
+            var params = {
+                aggregation: {
+                    '$sum': 'computableField'
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('sum').to.be.closeTo(sum, 0.000000000001);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('we get the collection with sum aggregation and' +
+               ' returns 0 when applied on a non numeric field', function(done) {
+            var params = {
+                aggregation: {
+                    '$sum': 'stringSortCut'
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('sum').to.be.equal(0);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('we get the collection with average aggregation and returns the expected average', function(done) {
+            var params = {
+                aggregation: {
+                    '$avg': 'computableField'
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('average').to.be.closeTo(sum / amount, 0.000000000001);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('we get the collection with average aggregation' +
+               ' and returns 0 when applied on a non numeric field', function(done) {
+            var params = {
+                aggregation: {
+                    '$avg': 'stringSortCut'
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('average').to.be.equal(0);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('returns an error when object is empty', function(done) {
+            var params = {
+                aggregation: {}
+            };
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.eventually.be.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 400);
+            })
             .should.eventually.be.fulfilled.notify(done);
         });
 
-
-        describe('if we get the collection with count aggregation', function() {
-
-            it('successes returning number elements in the collection', function(done) {
-                var params = {
-                    aggregation: {
-                        '$count': '*'
-                    }
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled.
-                then(function(result) {
-                    expect(result.data).to.have.property('count').to.be.equal(amount);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
+        it('returns an error when there are more than one operator', function(done) {
+            var params = {
+                aggregation: {
+                    '$avg': 'xxxxx',
+                    '$sum': 'xxxxx'
+                }
+            };
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.eventually.be.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 400);
+            })
+            .should.eventually.be.fulfilled.notify(done);
         });
 
-        describe('if we get the collection with count aggregation and a query', function() {
-
-            it('successes returning number elements that match the query', function(done) {
-                var params = {
-                    aggregation: {
-                        '$count': '*'
-                    },
-                    query: [{
-                        '$eq': {
-                            intField: 1000
-                        }
-                    }]
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled
-                .then(function(result) {
-                    expect(result.data).to.have.property('count').to.be.equal(1);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
-        });
-
-        describe('if we get the collection with sum aggregation', function() {
-
-            it('successes returning the expected sum', function(done) {
-                var params = {
-                    aggregation: {
-                        '$sum': 'computableField'
+        it('we get the collection with agreggation, query and sorts and ignores sort param', function(done) {
+            var params = {
+                sort: {
+                    intField: 'asc'
+                },
+                aggregation: {
+                    '$count': 'intField'
+                },
+                query: [{
+                    '$eq': {
+                        intField: 1000
                     }
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled
-                .then(function(result) {
-                    expect(result.data).to.have.property('sum').to.be.closeTo(sum, 0.000000000001);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
+                }]
+            };
 
-            it('returns 0 when applied on a non numeric field', function(done) {
-                var params = {
-                    aggregation: {
-                        '$sum': 'stringSortCut'
-                    }
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled
-                .then(function(result) {
-                    expect(result.data).to.have.property('sum').to.be.equal(0);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
-        });
-
-        describe('if we get the collection with average aggregation', function() {
-
-            it('successes returning the expected average', function(done) {
-                var params = {
-                    aggregation: {
-                        '$avg': 'computableField'
-                    }
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled
-                .then(function(result) {
-                    expect(result.data).to.have.property('average').to.be.closeTo(sum / amount, 0.000000000001);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
-
-            it('returns 0 when applied on a non numeric field', function(done) {
-                var params = {
-                    aggregation: {
-                        '$avg': 'stringSortCut'
-                    }
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled
-                .then(function(result) {
-                    expect(result.data).to.have.property('average').to.be.equal(0);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
-        });
-
-        describe('it we get collection with wrong aggregation parameters', function() {
-
-            it('returns an error when object is empty', function(done) {
-                var params = {
-                    aggregation: {}
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.rejected
-                .then(function(e) {
-                    expect(e).to.have.property('status', 400);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
-
-            it('returns an error when there are more than one operator', function(done) {
-                var params = {
-                    aggregation: {
-                        '$avg': 'xxxxx',
-                        '$sum': 'xxxxx'
-                    }
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.rejected
-                .then(function(e) {
-                    expect(e).to.have.property('status', 400);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
-        });
-
-        describe('if we get the collection with agreggation, and also with query and sort', function() {
-
-            it('successes ignoring sort param', function(done) {
-                var params = {
-                    sort: {
-                        intField: 'asc'
-                    },
-                    aggregation: {
-                        '$count': 'intField'
-                    },
-                    query: [{
-                        '$eq': {
-                            intField: 1000
-                        }
-                    }]
-                };
-                corbelDriver.resources.collection(COLLECTION)
-                .get(params)
-                .should.eventually.be.fulfilled
-                .then(function(result) {
-                    expect(result.data).to.have.property('count').to.be.equal(1);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
+            corbelDriver.resources.collection(COLLECTION)
+            .get(params)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response.data).to.have.property('count').to.be.equal(1);
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
         });
     });
 });
