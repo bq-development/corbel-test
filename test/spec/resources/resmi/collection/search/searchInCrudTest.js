d09b8b7924d7993d6ba265f76baa690086bb2575
ricardo <ricardo.martinez@bq.com>
2015-08-26 13:05:26 +0200

    searchInCrudTest refactor
    Change-Id: I82e1d90e6b4bd61c8ba44f7be1c161196bbc5f91

--- test/spec/resources/resmi/collection/search/searchInCrudTest.js
+++ test/spec/resources/resmi/collection/search/searchInCrudTest.js
@@ -1,75 +1,73 @@
-describe('In corbelDriver.resources module', function() {
-    var COLLECTION = 'test:searchableCollection';
+describe('In RESOURCES module', function() {
 
     describe('In RESMI module, testing search', function() {
         var corbelDriver;
+        var COLLECTION = 'test:searchableCollection';
 
         before(function() {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'];
         });
 
-        describe('CRUD in collection update search index when', function() {
-            
-            it.skip('successes returning elements satisfying the chain of search', function(done) {
-                var createdResourceId;
-                var random = Date.now();
+        it.skip('CRUD in collection update search index and' +
+               ' returns elements satisfying the chain of search', function(done) {
+            var createdResourceId;
+            var random = Date.now();
 
-                corbelDriver.resources.collection(COLLECTION)
-                .add({
-                    field1: 'Test' + random
-                })
-                .should.eventually.be.fulfilled
-                .then(function(id) {
-                    createdResourceId = id;
-                    var params = {
-                        search: 'Test' + random
-                    };
-                   
-                    return corbelDriver.resources.collection(COLLECTION)
+            corbelDriver.resources.collection(COLLECTION)
+            .add({
+                field1: 'Test' + random
+            })
+            .should.eventually.be.fulfilled
+            .then(function(id) {
+                createdResourceId = id;
+                var params = {
+                    search: 'Test' + random
+                };
+               
+                return corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.eventually.be.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.property('field1').to.be.equal('Test'+random);
+                expect(response).to.have.property('id').to.be.equal(createdResourceId);
+
+                return corbelDriver.resources.resource(COLLECTION, createdResourceId)
+                    .update({
+                        field1: 'OtherTest' + random,
+                    })
+                .should.eventually.be.fulfilled;
+            })
+            .then(function() {
+                var params = {
+                    search: 'Test' + random
+                };
+
+                return corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.eventually.be.fulfilled;
+            })
+            .then(function(response) {
+                expect(response.data[0]).to.have.property('field1').to.be.equal('Test'+random);
+                expect(response.data[0]).to.have.property('id').to.be.equal(createdResourceId);
+
+                return corbelDriver.resources.resource(COLLECTION, createdResourceId)
+                    .delete()
+                .should.eventually.be.fulfilled;
+            })
+            .then(function() {
+                var params = {
+                    search:  'othertest' + random
+                };
+
+                return corbelDriver.resources.collection(COLLECTION)
                     .get(params)
-                    .should.eventually.be.fulfilled;
-                })
-                .then(function(response) {
-                    expect(response).to.have.property('field1').to.be.equal('Test'+random);
-                    expect(response).to.have.property('id').to.be.equal(createdResourceId);
-
-                    return corbelDriver.resources.resource(COLLECTION, createdResourceId)
-                        .update({
-                            field1: 'OtherTest' + random,
-                        })
-                    .should.eventually.be.fulfilled;
-                })
-                .then(function() {
-                    var params = {
-                        search: 'Test' + random
-                    };
-
-                    return corbelDriver.resources.collection(COLLECTION)
-                    .get(params)
-                    .should.eventually.be.fulfilled;
-                })
-                .then(function(response) {
-                    expect(response.data[0]).to.have.property('field1').to.be.equal('Test'+random);
-                    expect(response.data[0]).to.have.property('id').to.be.equal(createdResourceId);
-
-                    return corbelDriver.resources.resource(COLLECTION, createdResourceId)
-                        .delete()
-                    .should.eventually.be.fulfilled;
-                })
-                .then(function() {
-                    var params = {
-                        search:  'othertest' + random
-                    };
-
-                    return corbelDriver.resources.collection(COLLECTION)
-                        .get(params)
-                    .should.eventually.be.fulfilled;
-                })
-                .then(function(response) {
-                    expect(response.data.length).to.be.equal(0);
-                })
-                .should.eventually.be.fulfilled.notify(done);
-            });
+                .should.eventually.be.fulfilled;
+            })
+            .then(function(response) {
+                expect(response.data.length).to.be.equal(0);
+            })
+            .should.eventually.be.fulfilled.notify(done);
         });
     });
 });
