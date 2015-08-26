f25ad89ba02f2c90787c62c3e89a26023848f212
Anthanh <anthanh.pham@bq.com>
2015-08-26 22:25:19 +0200

    grunt task removed, use 'npm link' instead
    Change-Id: Iedd577a31e6c8d4f7c108733421601fcad3f9284

--- src/common/clients.js
+++ src/common/clients.js
@@ -1,67 +1,80 @@
 'use strict';
 
-  var drivers = {};
-  var tokens = {};
-  var logins = {};
+var drivers = {};
+var tokens = {};
+var logins = {};
 
-  /**
-   * Create a new driver with the clientName creadentials.
-   * clientName credentials (corbelTest.CONFIG) are generated from .corbeltest config file or ENV variables
-   * @param  {string}       clientName clientName configuration to log with
-   * @return {corbelDriver} corbelDriver already autenticated
-   */
-  function login(clientName) {
-    if (!drivers[clientName]) {
-      var driverConfig = corbelTest.getConfig(clientName);
-      var savedConfig;
-      try {
-        savedConfig = JSON.parse(window.localStorage.getItem('driverconfig'));
-      } catch (e) {
-        console.warn('warn:parse:savedconfig');
-        savedConfig = {};
-      }
-
-      // Generate a driver config between descriptor and user saved config
-      driverConfig = _.extend(_.clone(driverConfig), savedConfig);
-      drivers[clientName] = corbel.getDriver(driverConfig);
-      var params = null;
-      if (driverConfig.username && driverConfig.password) {
-        params = {
-          claims: {
-            'basic_auth.username': driverConfig.username,
-            'basic_auth.password': driverConfig.password,
-            'scope': driverConfig.scopes
-          }
-        };
-      }
-      logins[clientName] = drivers[clientName].iam.token().create(params);
+/**
+ *
+ * @param  {string} clientName
+ * @return {object} a corbel config object
+ */
+function getConfig(clientName) {
+    var commonConfig = _.clone(corbelTest.CONFIG['COMMON']);
+    if (corbelTest.CONFIG.ENV) {
+        commonConfig.urlBase = commonConfig.urlBase.replace('{{ENV}}', corbelTest.CONFIG.ENV);
     }
-    return logins[clientName].then(function(response){
-      tokens[clientName] = response.data;
-    });
-  }
+    return _.extend(commonConfig, corbelTest.CONFIG[clientName]);
+}
 
-  /**
-   * Login with all `*_CLIENT` + `*_USER` defined in `.corbeltest`
-   * @return {Promise} A promise that resolves when all different clients/users are logged
-   */
-  function loginAll() {
+/**
+ * Create a new driver with the clientName creadentials.
+ * clientName credentials (corbelTest.CONFIG) are generated from .corbeltest config file or ENV variables
+ * @param  {string}       clientName clientName configuration to log with
+ * @return {corbelDriver} corbelDriver already autenticated
+ */
+function login(clientName) {
+    if (!drivers[clientName]) {
+        var driverConfig = corbelTest.getConfig(clientName);
+        var savedConfig;
+        try {
+            savedConfig = JSON.parse(window.localStorage.getItem('driverconfig'));
+        } catch (e) {
+            console.warn('warn:parse:savedconfig');
+            savedConfig = {};
+        }
+
+        // Generate a driver config between descriptor and user saved config
+        driverConfig = _.extend(_.clone(driverConfig), savedConfig);
+        drivers[clientName] = corbel.getDriver(driverConfig);
+        var params = null;
+        if (driverConfig.username && driverConfig.password) {
+            params = {
+                claims: {
+                    'basic_auth.username': driverConfig.username,
+                    'basic_auth.password': driverConfig.password,
+                    'scope': driverConfig.scopes
+                }
+            };
+        }
+        logins[clientName] = drivers[clientName].iam.token().create(params);
+    }
+    return logins[clientName].then(function(response) {
+        tokens[clientName] = response.data;
+    });
+}
+
+/**
+ * Login with all `*_CLIENT` + `*_USER` defined in `.corbeltest`
+ * @return {Promise} A promise that resolves when all different clients/users are logged
+ */
+function loginAll() {
     var promises = [];
 
     Object.keys(corbelTest.CONFIG).forEach(function(clientName) {
-      if (clientName.indexOf('_CLIENT') !== -1 || clientName.indexOf('_USER') !== -1) {
-        promises.push(login(clientName));
-      }
+        if (clientName.indexOf('_CLIENT') !== -1 || clientName.indexOf('_USER') !== -1) {
+            promises.push(login(clientName));
+        }
     });
 
     return Promise.all(promises);
-  }
+}
 
 
 module.exports = {
-  login : login, 
-  loginAll : loginAll,
-  drivers : drivers,
-  logins : logins,
-  tokens : tokens
+    login: login,
+    loginAll: loginAll,
+    drivers: drivers,
+    logins: logins,
+    tokens: tokens
 };
