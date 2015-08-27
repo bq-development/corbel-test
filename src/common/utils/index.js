d57d5c521fdf542a332bccc3f4f44525611c46ca
ricardo <ricardo.martinez@bq.com>
2015-08-27 10:46:18 +0200

    correct file for retry function
    Change-Id: I3414be24fdc7feb7daab283dfd9cd86716f5e309

--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -1,28 +1,3 @@
 'use strict';
-var q = require('q');
 
-var retry = function retry(retryFunction, maxRetries, retryPeriod, deferred, catches) {
-    deferred = deferred || q.defer();
-    catches = catches || [];
-
-    if (maxRetries < 1) {
-        deferred.reject(catches);
-    } else {
-        retryFunction().then(
-            function(response) {
-                deferred.resolve(response);
-            })
-            .catch(function(err) {
-                catches.push(err);
-                setTimeout(function() {
-                    retry(retryFunction, maxRetries - 1, retryPeriod, deferred, catches);
-                }, retryPeriod * 1000);
-            });
-    }
-
-    return deferred.promise;
-};
-
-module.exports = {
-	retry : retry
-};
+module.exports = require('./utilsCommon');
