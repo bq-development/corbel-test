9feec4d76f8631630febd57e7764c4e470d21c34
ricardo <ricardo.martinez@bq.com>
2015-08-27 18:33:05 +0200

    crudGroupTest created
    Change-Id: I68438e23dafd403c01a139ffde7728d87a5661f7

--- test/spec/iam/admin/domainApiErrorTest.js
+++ test/spec/iam/admin/domainApiErrorTest.js
@@ -15,7 +15,8 @@ describe('In IAM module, when testing domainAPIError', function() {
     describe('when performing domain CRUD operations', function() {
 
         it('should fail and return 422 when is requested to create an empty domain', function(done) {
-            corbelRootDriver.iam.domain().create({})
+            corbelRootDriver.iam.domain()
+            .create({})
             .should.be.eventually.rejected
             .then(function(e) {
                 expect(e).to.have.property('status', 422);
@@ -27,21 +28,25 @@ describe('In IAM module, when testing domainAPIError', function() {
         it('should fail and return 409 when is requested to create an existent domain', function(done) {
             var expectedDomain = getDomain();
 
-            corbelRootDriver.iam.domain().create(expectedDomain)
+            corbelRootDriver.iam.domain()
+            .create(expectedDomain)
             .should.be.eventually.fulfilled
             .then(function(id) {
                 expect(id).to.have.property('data', corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id);
 
-                return corbelRootDriver.iam.domain(id.data).get()
+                return corbelRootDriver.iam.domain(id.data)
+                .get()
                 .should.be.eventually.fulfilled;
             })
             .then(function(domain) {
                 var domainData = domain.data;
-                expectedDomain.id = corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id;
-                expect(domainData).to.include(expectedDomain);
                 domain.id = domainData.id.replace(/silkroad-qa:/g, '');
 
-                return corbelRootDriver.iam.domain().create(domain)
+                expectedDomain.id = corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id;
+                expect(domainData).to.include(expectedDomain);
+
+                return corbelRootDriver.iam.domain()
+                .create(domain)
                 .should.be.eventually.rejected;
             })
             .then(function(e) {
@@ -53,9 +58,10 @@ describe('In IAM module, when testing domainAPIError', function() {
 
         it('should fail and return 422 when is requested to create a domain with : inside the id', function(done) {
             var expectedDomain = getDomain();
-
             expectedDomain.id = 'silkroad-qa:test';
-            corbelRootDriver.iam.domain().create(expectedDomain)
+
+            corbelRootDriver.iam.domain()
+            .create(expectedDomain)
             .should.be.eventually.rejected
             .then(function(e) {
                 expect(e).to.have.property('status', 422);
@@ -69,24 +75,28 @@ describe('In IAM module, when testing domainAPIError', function() {
             var updateDomainId = 'anyDomain:test';
             var domainId;
 
-            corbelRootDriver.iam.domain().create(expectedDomain)
+            corbelRootDriver.iam.domain()
+            .create(expectedDomain)
             .should.be.eventually.fulfilled
             .then(function(id) {
                 domainId = id.data;
                 expect(domainId).to.be.equals(corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id);
 
-                return corbelRootDriver.iam.domain(domainId).update({domainId:updateDomainId})
+                return corbelRootDriver.iam.domain(domainId)
+                .update({domainId:updateDomainId})
                 .should.be.eventually.fulfilled;
             })
             .then(function() {
-                return corbelRootDriver.iam.domain(updateDomainId).get()
+                return corbelRootDriver.iam.domain(updateDomainId)
+                .get()
                 .should.be.eventually.rejected;
             })
             .then(function(e) {
                 expect(e).to.have.property('status', 404);
                 expect(e).to.have.deep.property('data.error', 'not_found');
 
-                return corbelRootDriver.iam.domain(domainId).remove()
+                return corbelRootDriver.iam.domain(domainId)
+                .remove()
                 .should.be.eventually.fulfilled;
             })
             .should.be.eventually.fulfilled.and.notify(done);
@@ -103,7 +113,8 @@ describe('In IAM module, when testing domainAPIError', function() {
                 }]
             };
 
-            corbelRootDriver.iam.domain().getAll(params)
+            corbelRootDriver.iam.domain()
+            .getAll(params)
             .should.be.eventually.rejected
             .then(function(e) {
                 expect(e).to.have.property('status', 400);
@@ -114,9 +125,7 @@ describe('In IAM module, when testing domainAPIError', function() {
     });
 
     describe('for client management', function() {
-
         var timeStamp, domainId, scope1, scope2;
-
         var getScope = function(id) {
             return {
                 id: id,
@@ -129,18 +138,21 @@ describe('In IAM module, when testing domainAPIError', function() {
         before(function(done) {
             timeStamp = Date.now();
 
-            return corbelRootDriver.iam.scope().create(getScope('TestScope1_' + timeStamp))
+            return corbelRootDriver.iam.scope()
+            .create(getScope('TestScope1_' + timeStamp))
             .should.be.eventually.fulfilled
             .then(function(id) {
                 scope1 = id.data;
 
-                return corbelRootDriver.iam.scope().create(getScope('TestScope2_' + timeStamp))
+                return corbelRootDriver.iam.scope()
+                .create(getScope('TestScope2_' + timeStamp))
                 .should.be.eventually.fulfilled;
             })
             .then(function(id) {
                 scope2 = id.data;
 
-                return corbelRootDriver.iam.domain().create({
+                return corbelRootDriver.iam.domain()
+                .create({
                     id: 'TestDomain_' + timeStamp,
                     scopes: [scope1]
                 })
@@ -153,14 +165,17 @@ describe('In IAM module, when testing domainAPIError', function() {
         });
 
         after(function(done) {
-            return corbelRootDriver.iam.domain(domainId).remove()
+            return corbelRootDriver.iam.domain(domainId)
+            .remove()
             .should.be.eventually.fulfilled
             .then(function() {
-                return corbelRootDriver.iam.scope(scope1).remove(scope1)
+                return corbelRootDriver.iam.scope(scope1)
+                .remove(scope1)
                 .should.be.eventually.fulfilled;
             })
             .then(function() {
-                return corbelRootDriver.iam.scope(scope2).remove(scope2)
+                return corbelRootDriver.iam.scope(scope2)
+                .remove(scope2)
                 .should.be.eventually.fulfilled;
             })
             .should.be.eventually.fulfilled.and.notify(done);
@@ -168,7 +183,8 @@ describe('In IAM module, when testing domainAPIError', function() {
 
         it('should fail with FORBIDDEN (403) when tries to create a client with more scopes than its domain',
         function(done) {
-            return corbelRootDriver.iam.client(domainId).create({
+            return corbelRootDriver.iam.client(domainId)
+            .create({
                 name: 'TestClient_' + timeStamp,
                 scopes: [scope1, scope2]
             })
