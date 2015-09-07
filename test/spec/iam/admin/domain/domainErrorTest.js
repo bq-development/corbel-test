b781b0c742d47f99b069de67aa3e2b1c417e0049
ricardo <ricardo.martinez@bq.com>
2015-09-07 16:33:08 +0200

    [Patch 3] iam admin tests added
    Change-Id: I94e4831c8d542950aa407ac9eb923566b04f634e

---
+++ test/spec/iam/admin/domain/domainErrorTest.js
@@ -0,0 +1,195 @@
+describe('In IAM module', function() {
+    var corbelDriver;
+    var corbelDefaultDriver;
+
+    before(function() {
+        corbelDriver = corbelTest.drivers['ROOT_CLIENT'];
+        corbelDefaultDriver = corbelTest.drivers['DEFAULT_CLIENT'];
+    });
+
+    describe('when performing domain CRUD operations', function() {
+
+        it('an error 422 is returned when try to create an empty domain', function(done) {
+            corbelDriver.iam.domain()
+            .create({})
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 422);
+                expect(e).to.have.deep.property('data.error', 'invalid_entity');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 409 is returned when try to create an existent domain', function(done) {
+            var expectedDomain = corbelTest.common.iam.getDomain();
+
+            corbelDriver.iam.domain()
+            .create(expectedDomain)
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                expect(id).to.be.equal(corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id);
+
+                return corbelDriver.iam.domain(id)
+                .get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(domain) {
+                expect(domain).to.have.deep.property('data.id', corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id);
+
+                return corbelDriver.iam.domain()
+                .create(expectedDomain)
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 409);
+                expect(e).to.have.deep.property('data.error', 'entity_exists');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 422 is returned when try to create a domain with : inside the id', function(done) {
+            var expectedDomain = corbelTest.common.iam.getDomain();
+            expectedDomain.id = 'silkroad-qa:test';
+
+            corbelDriver.iam.domain()
+            .create(expectedDomain)
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 422);
+                expect(e).to.have.deep.property('data.error', 'invalid_domain_id');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 401 is returned when try to create a domain without authorization', function(done) {
+            var expectedDomain = corbelTest.common.iam.getDomain();
+            expectedDomain.id = 'silkroad-qa:test';
+
+            corbelDefaultDriver.iam.domain()
+            .create(expectedDomain)
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 404 is returned when try to modify the domain id', function(done) {
+            var expectedDomain = corbelTest.common.iam.getDomain();
+            var updateDomainId = 'anyDomain:test';
+            var domainId;
+
+            corbelDriver.iam.domain()
+            .create(expectedDomain)
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                domainId = id;
+                expect(domainId).to.be.equals(corbelTest.CONFIG.DOMAIN  + ':' + expectedDomain.id);
+
+                return corbelDriver.iam.domain(domainId)
+                .update({domainId:updateDomainId})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.iam.domain(updateDomainId)
+                .get()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+
+                return corbelDriver.iam.domain(domainId)
+                .remove()
+                .should.be.eventually.fulfilled;
+            })
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('an error 401 is returned when try to get a domain which does not exist', function(done) {
+            var id = Date.now();
+
+            corbelDriver.iam.domain(id)
+            .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 401 is returned when try to create a domain without authorization', function(done) {
+            var id = Date.now();
+
+            corbelDefaultDriver.iam.domain(id)
+            .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 400 is returned when try to get all domains using invalid query params',
+        function(done) {
+            var params = {
+                query: [{
+                    '$sum': {
+                        description: 'domainDescription'
+                    }
+                }]
+            };
+
+            corbelDriver.iam.domain()
+            .getAll(params)
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 400);
+                expect(e).to.have.deep.property('data.error', 'invalid_query');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 401 is returned when try to update a domain without authorization', function(done) {
+            var id = Date.now();
+
+            corbelDefaultDriver.iam.domain(id)
+            .update(corbelTest.common.iam.getDomain())
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 422 is returned when try to update a domain with malformed entity', function(done) {
+            var id = Date.now();
+
+            corbelDriver.iam.domain('Pepe')
+            .update('asdf')
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 422);
+                expect(e).to.have.deep.property('data.error', 'invalid_entity');
+            })
+            .should.notify(done);
+        });
+
+        it('an error 401 is returned when try to delete a domain without authorization', function(done) {
+            var id = Date.now();
+
+            corbelDefaultDriver.iam.domain(id)
+            .remove()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+    });
+});
---
+++ test/spec/iam/admin/scopes/scopesTest.js
@@ -0,0 +1,56 @@
+describe('In IAM module', function() {
+
+    describe('when performing scopes CRUD operations', function() {
+        var corbelDriver;
+        var scopeId = 'TestScope' + Date.now(); 
+
+        before(function() {
+            corbelDriver = corbelTest.drivers['ROOT_CLIENT'];
+        });
+
+        it('a scope is created', function(done) {
+            var expectedScope = corbelTest.common.iam.getScope(scopeId);
+
+            corbelDriver.iam.scope()
+            .create(expectedScope)
+            .should.eventually.be.fulfilled
+            .then(function(id) {
+                expect(id).to.be.equals(expectedScope.id);
+
+                return corbelDriver.iam.scope(expectedScope.id)
+                .get()
+                .should.eventually.be.fulfilled;
+            })
+            .then(function(scope) {
+                expect(scope).to.have.deep.property('data.id', expectedScope.id);
+                expect(scope).to.have.deep.property('data.audience', expectedScope.audience);
+                expect(scope).to.have.deep.property('data.rules[0].testRule', expectedScope.rules[0].testRule);
+                expect(scope).to.have.deep.property('data.parameters.a', expectedScope.parameters.a);
+            })
+            .should.notify(done);
+        });
+
+        it('a scope is removed', function(done) {
+            var scope = corbelTest.common.iam.getScope(scopeId);
+
+            corbelDriver.iam.scope()
+            .create(scope)
+            .should.eventually.be.fulfilled
+            .then(function(id) {
+                return corbelDriver.iam.scope(scope.id)
+                .remove(id)
+                .should.eventually.be.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.iam.scope(scope.id)
+                .get()
+                .should.eventually.be.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+    });
+});
