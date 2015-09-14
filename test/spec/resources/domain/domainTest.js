e6029640bf3f16d9118979da0a98c8c14015d7e5
ricardo <ricardo.martinez@bq.com>
2015-09-14 14:29:26 +0200

    domain test added
    Change-Id: If4a87d9a41e9322c548a797e42b22641b30efe17

---
+++ test/spec/resources/domain/domainTest.js
@@ -0,0 +1,17 @@
+describe('In RESOURCES module', function() {
+
+    describe('while testing domain formation in URL', function() {
+        var corbelDriver;
+        before(function() {
+          corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+        });
+
+        it('domain appears in URL', function(done) {
+
+        });
+
+        it('untenticated appears if the domain has not been assigned', function(done) {
+
+        });
+    });
+});
