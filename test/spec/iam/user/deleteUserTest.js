4079c705a99709286a53d349616014fc7b158821
ricardo <ricardo.martinez@bq.com>
2015-09-17 10:17:12 +0200

    deleteUserTests added
    Change-Id: Ibf84332e6b65fb2607fea7ee4ad112f435384f4b

---
+++ test/spec/iam/user/deleteUserTest.js
@@ -0,0 +1,84 @@
+describe('In IAM module', function() {
+
+    describe('while testing delete user', function() {
+        var corbelDriver;
+        var userId;
+        var domainEmail = '@funkifake.com';
+        var userDeleteTest = {
+            'firstName': 'userDelete',
+            'email': 'user.delete.',
+            'username': 'user.delete.'
+        };
+
+        before(function() {
+            corbelDriver = corbelTest.drivers['ADMIN_USER'];
+        });
+
+        beforeEach(function(done) {
+            var random = Date.now();
+
+            corbelDriver.iam.user()
+            .create({
+                'firstName': userDeleteTest.firstName,
+                'email': userDeleteTest.email + random + domainEmail,
+                'username': userDeleteTest.username + random + domainEmail
+            })
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                userId = id;
+            })
+            .should.notify(done);
+        });
+
+        it('basic user is deleted', function(done) {
+
+            corbelDriver.iam.user(userId)
+            .delete()
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.iam.user(userId)
+                .get()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e){
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('basic user and his devices are deleted', function(done) {
+            var deviceId;
+
+            var device = {
+                notificationUri: '123',
+                uid: '123',
+                name: 'My device',
+                type: 'Android',
+                notificationEnabled: true
+            };
+
+            corbelDriver.iam.user(userId)
+            .registerDevice(device)
+            .should.eventually.be.fulfilled
+            .then(function(id) {
+                deviceId = id;
+
+                return corbelDriver.iam.user(userId)
+                .delete()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.iam.user(userId)
+                .getDevice(deviceId)
+                .should.eventually.be.rejected;
+            }).
+            then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+                expect(e).to.have.deep.property('data.errorDescription', 'Not found');
+            })
+            .should.notify(done);
+        });
+    });
+});
