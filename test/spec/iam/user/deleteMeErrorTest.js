e03d22f8efac42184b423973143523aca617714a
samuelmartingc <samuelmartingc@gmail.com>
2015-09-21 13:40:03 +0200

    fix in deleteMe tests
    Change-Id: If5c608a825b5e06fb1ea675eed4753a59bd99d67

--- test/spec/iam/user/deleteMeErrorTest.js
+++ test/spec/iam/user/deleteMeErrorTest.js
@@ -14,12 +14,9 @@ describe('In IAM module', function() {
             'password': 'passDeleteMe'
         };
 
-        before(function() {
+        beforeEach(function(done) {
             corbelDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
             corbelDefaultDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
-        });
-
-        beforeEach(function(done) {
             var random = Date.now();
 
             corbelDriver.iam.user()
@@ -62,6 +59,20 @@ describe('In IAM module', function() {
             corbelDefaultDriver.iam.user()
             .deleteMe()
             .should.be.eventually.rejected
+            .then(function(e){
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .then(function(){
+                return corbelDriver.iam.user()
+                .deleteMe()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.iam.user(userId)
+                .get()
+                .should.be.eventually.rejected;
+            })
             .then(function(e){
                 expect(e).to.have.property('status', 401);
                 expect(e).to.have.deep.property('data.error', 'unauthorized');
