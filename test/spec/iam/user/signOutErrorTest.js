b3edd8299024a57922ff23a67d41a811d92d2d27
ricardo <ricardo.martinez@bq.com>
2015-09-22 10:06:24 +0200

    signOut tests added
    Change-Id: Idff808bc88d9157bee1756284b2cf600757e657a

---
+++ test/spec/iam/user/signOutErrorTest.js
@@ -0,0 +1,21 @@
+describe('In IAM module', function() {
+
+    describe('while testing signOut', function() {
+        var corbelRootDriver;
+
+        before(function(){
+            corbelRootDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+        });
+
+        it('an error [401] is returned while trying to signOut a non logged user', function(done) {
+            corbelRootDriver.iam.user('me')
+            .signOut()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+    });
+});
