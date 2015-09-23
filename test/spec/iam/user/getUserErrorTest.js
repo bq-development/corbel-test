b85dcc81bccfc03fd13422b934593e3b51a9dfc6
ricardo <ricardo.martinez@bq.com>
2015-09-23 17:40:48 +0200

    getUserErrorTest added
    Change-Id: I0d51f62ac539ae1aa2bec4ca3425b317e4aea3d9

---
+++ test/spec/iam/user/getUserErrorTest.js
@@ -0,0 +1,69 @@
+describe('In IAM module', function() {
+
+    describe('while testing get user', function() {
+        var corbelDriver;
+        var corbelRootDriver;
+        var random;
+        var userId;
+        var emailDomain = '@funkifake.com';
+        var user = {
+            'firstName': 'userGet',
+            'email': 'user.get.',
+            'username': 'user.get.',
+            'password': 'pass'
+        };
+
+        before(function() {
+            corbelRootDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+            corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+        });
+
+        it('an error is returned while trying to get an user with unauthorized driver using "me"', function(done) {
+
+            corbelRootDriver.iam.user('me')
+            .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('an error is returned while trying to get an user with unauthorized driver using id', function(done) {
+
+            corbelDriver.iam.user(userId)
+            .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('an error is returned while trying to get an unexistent user', function(done) {
+
+            corbelRootDriver.iam.user('unexistent')
+            .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('an error is returned while trying to get all users with unauthorized driver', function(done) {
+
+            corbelDriver.iam.user()
+            .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+    });
+});
