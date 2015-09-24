bd860aab73e2e6d99a9a37fe04f7f78160250a1f
samuelmartingc <samuelmartingc@gmail.com>
2015-09-24 11:12:50 +0200

    migrated test for devices
    Change-Id: I8a068888268f19f870161bdabbed47469c76385a

---
+++ test/spec/iam/user/devices/devicesErrorUnauthorizedTest.js
@@ -0,0 +1,97 @@
+describe('In IAM module', function() {
+    var userId;
+    var corbelDriver, corbelRootDriver;
+    var random;
+    var user = {
+        'firstName': 'userGet',
+        'email': 'user.get.',
+        'username': 'user.get.',
+        'password': 'pass'
+    };
+    var emailDomain = '@funkifake.com';
+
+    before(function() {
+        corbelRootDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+    });
+
+    describe('while testing unauthorized errors in devices', function() {
+
+        beforeEach(function(done) {
+            corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+            random = Date.now();
+
+            corbelDriver.iam.users()
+            .create({
+                'firstName': user.firstName + random,
+                'email': user.email + random + emailDomain,
+                'username': user.username + random + emailDomain,
+                'password': user.password
+            })
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                userId = id;
+
+                return corbelTest.common.clients.loginUser
+                (corbelDriver, user.username + random + emailDomain, user.password)
+                .should.eventually.be.fulfilled;
+            })
+            .should.notify(done);
+        });
+
+        afterEach(function(done) {
+            corbelRootDriver.iam.user(userId)
+            .delete()
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelRootDriver.iam.user(userId)
+                .get()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+        var device = {
+            notificationUri: '123',
+            uid: '123',
+            name: 'device',
+            type: 'Android',
+            notificationEnabled: true
+        };
+
+        it('fails with 401 when request register user devices', function(done) {
+            corbelDriver.iam.user(userId)
+            .registerDevice(device)
+            .should.eventually.be.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('fails with 401 when request retrieve user devices with access token not admin', function(done) {
+            corbelDriver.iam.user(userId)
+            .getDevices()
+            .should.eventually.be.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+
+        it('fails with 401 when request retrieve a specific user decive with not admin access token', function(done) {
+            corbelDriver.iam.user(userId)
+            .getDevices('randomDeviceId')
+            .should.eventually.be.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+    });
+});
