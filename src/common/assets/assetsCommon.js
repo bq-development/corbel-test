58ad3156d0ed855e1c9e42d8eb997bbdb3f6892f
vipereybq <victor.perezrey@bq.com>
2015-10-01 14:41:10 +0200

    [Patch 8][Corbel-test] SILKROAD-1784 > SILKROAD-1816 Token POST && assets test migration
    Change-Id: I671f1ce868f05b86cad7b9e5f15f665453ec9ffe

--- src/common/assets/assetsCommon.js
+++ src/common/assets/assetsCommon.js
@@ -19,7 +19,31 @@ function getAsset(scopes) {
     };
 }
 
+function createMultipleAssets(driver, count, userId) {
+
+    var promises = [];
+    var createAsset = function(customId, userId) {
+        return {
+            userId: userId,
+            name: 'createAssetTest' + customId,
+            productId: String(Date.now()),
+            expire: getExpire(),
+            active: true,
+            scopes: ['custom:test;type=Custom;customId=' + customId]
+        };
+    };
+
+    for (var i = 0; i <= count; i++) {
+        var asset = createAsset(i, userId);
+        var promise = driver.assets().create(asset);
+        promises.push(promise);
+    }
+    return Promise.all(promises);
+}
+
+
 module.exports = {
     getExpire : getExpire,
-    getAsset : getAsset
+    getAsset : getAsset,
+    createMultipleAssets : createMultipleAssets
 };
