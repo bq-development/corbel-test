20e4f2bed548ffbe532da79deab9ba34a49651a1
vipereybq <victor.perezrey@bq.com>
2015-10-01 14:41:10 +0200

    [Patch 6][Corbel-test] SILKROAD-1784 > SILKROAD-1816 Token POST && assets test migration
    Change-Id: I671f1ce868f05b86cad7b9e5f15f665453ec9ffe

--- test/spec/assets/assetsAccessErrorTest.js
+++ test/spec/assets/assetsAccessErrorTest.js
@@ -1,17 +1,19 @@
 describe('In ASSETS module', function() {
-    var corbelDriver;
+    describe('when client with no GET/assets scopes asks for assets', function(){
+        var corbelDriver;
 
-    before(function() {
-        corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
-    });
-
-    it('when a client asks for assets without authorization, server replies with an unauthorized code (401)',
-        function(done) {
-            corbelDriver.assets().access()
-                .should.be.eventually.rejected
-                .then(function(e) {
-                    expect(e).to.have.property('status', 401);
-                })
-                .should.notify(done);
+        before(function() {
+            corbelDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
         });
+
+        it('request is rejected due to authorization reasons', function(done) {
+            corbelDriver.assets().access()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+    });
 });
