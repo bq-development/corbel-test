6a5921b816e758118177be83e03ffe875871dbeb
vipereybq <victor.perezrey@bq.com>
2015-10-02 16:36:40 +0200

    [Patch 3][Corbel-test] SILKROAD-1784 > SILKROAD-1816 Token POST && assets test migration
    Change-Id: I61b3e4e8a7818cf05e4183687fa2cc3084fa3e41

--- test/spec/assets/assetsAccessErrorTest.js
+++ test/spec/assets/assetsAccessErrorTest.js
@@ -1,17 +1,61 @@
 describe('In ASSETS module', function() {
-    var corbelDriver;
 
-    before(function() {
-        corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+    describe('when a client asks for assets/access', function(){
+        var corbelDriver;
+
+        before(function() {
+            corbelDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+        });
+
+        it('request is rejected due to authorization reasons', function(done) {
+            corbelDriver.assets().access()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
     });
 
-    it('when a client asks for assets without authorization, server replies with an unauthorized code (401)',
-        function(done) {
-            corbelDriver.assets().access()
-                .should.be.eventually.rejected
-                .then(function(e) {
-                    expect(e).to.have.property('status', 401);
-                })
-                .should.notify(done);
+    describe('when no user is logged-in', function() {
+
+        var corbelDriver;
+        var user;
+
+        before(function(done) {
+            corbelDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+            corbelTest.common.iam.createUsers(corbelDriver, 1)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                user = response[0];
+                return corbelTest.common.clients.loginUser( corbelDriver, user.username, user.password)
+                 .should.be.eventually.fulfilled;
+            })
+            .should.notify(done);
         });
+
+        after(function(done){
+            corbelDriver.iam.user(user.id).delete()
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('asset is not retrieved due to authorization reasons', function(done) {
+            corbelDriver.assets().access()
+            .should.be.eventually.fulfilled
+            .then(function(response){
+                return corbelDriver.iam.user(user.id).disconnect()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response){
+                return corbelDriver.assets().access()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 401);
+                expect(e).to.have.deep.property('data.error', 'unauthorized');
+            })
+            .should.notify(done);
+        });
+    });
 });
