27f348576b25291f2d8f5fe087a20f21cb997ccf
ricardo <ricardo.martinez@bq.com>
2015-10-05 10:52:13 +0200

    [Patch 3]partial put relations test improved
    Change-Id: Ib15eaeab9c0d55c4021929dc9a3ebe8a5b46c176

--- test/spec/resources/resmi/relation/creation/createRelationTest.js
+++ test/spec/resources/resmi/relation/creation/createRelationTest.js
@@ -1,6 +1,6 @@
 describe('In RESOURCES module', function() {
 
-    describe('In RESMI module, testing create relation', function() {
+    describe('In RESMI module, when testing relation creation', function() {
         var corbelDriver;
         var COLLECTION_A = 'test:CorbelJSTestRelation' + Date.now();
         var COLLECTION_B = 'test:CorbelJSTestRelation-dest' + Date.now();
@@ -16,9 +16,9 @@ describe('In RESOURCES module', function() {
         beforeEach(function(done) {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
             corbelDriver.resources.collection(COLLECTION_A)
-            .add(TEST_OBJECT).
-            should.eventually.be.fulfilled.
-            then(function(id) {
+            .add(TEST_OBJECT)
+            .should.eventually.be.fulfilled
+            .then(function(id) {
                 idResourceA = id;
 
                 return corbelDriver.resources.collection(COLLECTION_B)
@@ -36,7 +36,7 @@ describe('In RESOURCES module', function() {
             .should.be.eventually.fulfilled
             .then(function() {
                 return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
-                    .delete();
+                .delete();
             }).
             should.eventually.be.fulfilled.notify(done);
         });
@@ -44,7 +44,8 @@ describe('In RESOURCES module', function() {
         it('a registry in the relation is created and updated', function(done){
             corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
             .add(idResourceB, {
-                myextrafield : 'test'
+                myextrafield : 'test',
+                secondField : 'second'
             })
             .should.be.fulfilled
             .then(function(response){
@@ -54,11 +55,13 @@ describe('In RESOURCES module', function() {
             .should.be.fulfilled
             .then(function(response){
                 expect(response).to.have.deep.property('data.myextrafield', 'test');
+                expect(response).to.have.deep.property('data.secondField', 'second');
             })
             .then(function(){
                 return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
                 .add(idResourceB, {
-                    myextrafield : 'updatedField'
+                    myextrafield : 'updatedField',
+                    thirdField : 'third'
                 })
                 .should.be.eventually.fulfilled;
             })
@@ -66,9 +69,27 @@ describe('In RESOURCES module', function() {
                 return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
                     .get(idResourceB);
             })
-            .should.be.fulfilled
+            .should.be.eventually.fulfilled
             .then(function(response){
                 expect(response).to.have.deep.property('data.myextrafield', 'updatedField');
+                expect(response).to.have.deep.property('data.secondField', 'second');
+                expect(response).to.have.deep.property('data.thirdField', 'third');
+
+                return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
+                .add(idResourceB, {
+                    myextrafield : null
+                })
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response){
+                return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
+                    .get(idResourceB);
+            })
+            .should.be.eventually.fulfilled
+            .then(function(response){
+                expect(response).not.to.have.deep.property('data.myextrafield');
+                expect(response).to.have.deep.property('data.secondField', 'second');
+                expect(response).to.have.deep.property('data.thirdField', 'third');
             })
             .should.notify(done);
         });
