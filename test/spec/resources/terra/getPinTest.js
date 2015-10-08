b07c9d1bba0efc7bed70990f124fe597e059970b
ricardo <ricardo.martinez@bq.com>
2015-10-08 12:39:23 +0200

    [Patch 2]terra subscription tests added
    Change-Id: Icfa442c37d5e198d969a79f5ecb79b93da4df060

---
+++ test/spec/resources/terra/getPinTest.js
@@ -0,0 +1,35 @@
+describe('In RESOURCES module, in TERRA rem', function() {
+
+    describe('while testing get Pin for Terra subscription', function() {
+        var corbelDriver;
+        var TERRA_PINCOLLECTION = 'books:ChilePin';
+        var rightMobileNumber = '56989223009';
+        var wrongMobileNumber = '56989';
+
+        before(function() {
+            corbelDriver = corbelTest.drivers['ADMIN_CLIENT'];
+        });
+
+        it('a pin can be gotten', function(done) {
+
+            corbelDriver.resources.resource(TERRA_PINCOLLECTION, rightMobileNumber)
+            .update()
+            .should.be.eventually.fulfilled
+            .then(function(response){
+                expect(response).to.have.property('status', 200); 
+            })
+            .should.notify(done);
+        });
+
+        it('an error [404] is returned while trying to get the pin with invalid number', function(done) {
+
+            corbelDriver.resources.resource(TERRA_PINCOLLECTION, wrongMobileNumber)
+            .update()
+            .should.be.eventually.rejected
+            .then(function(response){
+                expect(response).to.have.property('status', 404); 
+            })
+            .should.notify(done);
+        });
+    });
+});
