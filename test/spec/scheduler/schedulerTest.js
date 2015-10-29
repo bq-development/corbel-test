71ebff83eaebb702d5d9dd6dda25098995baa00c
vipereybq <victor.perezrey@bq.com>
2015-10-29 11:39:38 +0100

    [Patch 2] Scheduler tests added
    Change-Id: I17814225dc63ddd510d090bcbefc58a91fb81f24

---
+++ test/spec/scheduler/schedulerTest.js
@@ -0,0 +1,162 @@
+describe('In SCHEDULER module', function() {
+    var corbelDriver;
+    var taskId, task;
+
+    before(function() {
+      corbelDriver = corbelTest.drivers['ADMIN_CLIENT'].clone();
+    });
+            
+    describe('when a new absolute task is scheduled', function(){
+
+        before(function(done) {
+            taskId = 'scheduler:TestAbsoluteScheduler:' + Date.now();
+            task = {
+                'taskId':taskId,
+                'taskDefinition': {
+                    'message': 'message' 
+                },
+                'time': {
+                    'type': 'ABSOLUTE',
+                    'moment':'00:01'
+                }
+            };
+
+            corbelDriver.scheduler.task().create(task)
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('resulting task can be retrieved', function(done) {
+            corbelDriver.scheduler.task(taskId).get()
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.taskId', taskId);
+                expect(response).to.have.deep.property('data.period', 'P1D');
+            })
+            .should.notify(done);
+        });
+
+        it('the task can be rescheduled', function(done) {
+
+            var originalNextExecutionValue;
+            var params = {
+                'time': {
+                    'type': 'ABSOLUTE',
+                    'moment':'23:30'
+                }
+            };
+
+            corbelDriver.scheduler.task(taskId).get()
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                originalNextExecutionValue = response.data.nextExecution;
+                expect(response).to.have.deep.property('data.taskId', taskId);
+                return corbelDriver.scheduler.task(taskId).update(params)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.scheduler.task(taskId).get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.period', 'P1D');
+                expect(response).to.have.deep.property('data.nextExecution').not.equals(originalNextExecutionValue);
+
+            })
+            .should.notify(done);
+        });
+
+        it('the task can be deleted', function(done) {
+
+            corbelDriver.scheduler.task(taskId).delete()
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.scheduler.task(taskId).get()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+	});
+
+    describe('when a new relative task is scheduled', function(){
+
+        before(function(done) {
+            taskId = 'scheduler:TestRelativeScheduler:' + Date.now();
+            task = {
+                'taskId':taskId,
+                'time': {
+                    'type': 'RELATIVE',
+                    'delay':'P0D',
+                    'period':'PT72H'
+                },
+                'taskDefinition': {
+                    'message': 'message' 
+                },
+            };
+
+            corbelDriver.scheduler.task().create(task)
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('resulting task can be retrieved', function(done) {
+            corbelDriver.scheduler.task(taskId).get()
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.taskId', taskId);
+                expect(response).to.have.deep.property('data.period', task.time.period);
+            })
+            .should.notify(done);
+        });
+
+        it('the task can be rescheduled', function(done) {
+
+            var originalNextExecutionValue;
+            var params = {
+                'time': {
+                    'type': 'RELATIVE',
+                    'delay':'P0D',
+                    'period':'PT48H'
+                }
+            };
+
+            corbelDriver.scheduler.task(taskId).get()
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.taskId', taskId);
+                return corbelDriver.scheduler.task(taskId).get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(data) {
+                originalNextExecutionValue = data.nextExecution;
+                return corbelDriver.scheduler.task(taskId).update(params)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.scheduler.task(taskId).get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.period', params.time.period);
+            })
+            .should.notify(done);
+        });
+
+        it('the task can be deleted', function(done) {
+
+            corbelDriver.scheduler.task(taskId).delete()
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.scheduler.task(taskId).get()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+	});
+});
