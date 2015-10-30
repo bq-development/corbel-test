3b7e06ef71a1d65152c77f85ff8a9a977d9f4d8c
laurasada <laura.sada@bq.com>
2015-10-30 15:47:28 +0100

    basic login tests
    Change-Id: I27f9e7eaac81efc22850f68b4c9c8fe74e666a37

---
+++ test/spec/iam/token/basicLoginTest.js
@@ -0,0 +1,60 @@
+describe('In IAM module', function() {
+
+    var corbelDriver;
+    var userId;
+    var corbelDriverAdmin;
+    var random = Date.now();
+    var domainEmail = '@funkifake.com';
+    var user = {
+        'firstName': 'createUserIam',
+        'email': 'createUserIam.iam.' + random + domainEmail,
+        'username': 'createUserIam.iam.' + random + domainEmail,
+        'password': 'myPassword'
+    };
+
+    before(function() {
+        corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+        corbelDriverAdmin = corbelTest.drivers['ADMIN_CLIENT'].clone();
+    });
+
+    after(function(done) {
+        corbelDriverAdmin.iam.user(userId)
+            .delete()
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriverAdmin.iam.user(userId)
+                    .get()
+                    .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+    });
+
+    it('a new user can be created and logged with basic login', function(done) {
+
+        corbelDriverAdmin.iam.users()
+            .create(user)
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                userId = id;
+                return corbelTest.common.clients
+                    .loginUser(corbelDriver, user.email, user.password)
+                    .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriverAdmin.iam.user(userId)
+                    .get()
+                    .should.be.eventually.fulfilled;
+            })
+            .then(function(createdUser) {
+                expect(createdUser).to.have.deep.property('data.firstName', user.firstName);
+                expect(createdUser).to.have.deep.property('data.email', user.email.toLowerCase());
+                expect(createdUser).to.have.deep.property('data.username', user.username);
+                expect(createdUser).not.to.contain.keys('password', 'salt');
+            })
+            .should.notify(done);
+    });
+});
