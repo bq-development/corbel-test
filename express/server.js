ca43103157070375784c44b37e3a2b72889182e8
ricardo <ricardo.martinez@bq.com>
2015-11-10 21:23:58 +0100

    [Patch 2]resetPassword test y express server
    Change-Id: Ifa3399ae34af3927941ed9e124f5edb2362aa83b

---
+++ express/server.js
@@ -0,0 +1,119 @@
+var express = require('express');
+var request = require('request');
+
+var API_EMAIL_ENDPOINT = 'http://api.guerrillamail.com/ajax.php';
+var API_RANDOM_EMAIL_SUFFIX = 'f=get_email_address';
+var API_SET_EMAIL_SUFFIX = 'f=set_email_user';
+var API_CHECK_EMAIL_SUFFIX = 'f=check_email&seq=1';
+var API_GET_EMAIL_SUFFIX = 'f=fetch_email';
+// Set up the express server
+var app = express();
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/email/randomemail', function(req, res){
+    request(API_EMAIL_ENDPOINT + '?' + API_RANDOM_EMAIL_SUFFIX, function (error, response, body) {
+        var cookiesHeader = {};
+        if (!error && response.statusCode === 200) {
+            response.headers['set-cookie'].map(function(cookieArray){
+                cookieArray.split(';').map(function(cookie){
+                    var cookieData = cookie.split('=');
+                    cookiesHeader[cookieData[0]] = cookieData[1] || '';
+                });
+            });
+
+            res.send({cookies: cookiesHeader, emailData: JSON.parse(body)});
+        }else{
+          res.status(500).send(error);
+        }
+    });
+});
+
+app.get('/email/setemail', function(req, res){
+    var credentials = req.query.token;
+    var userEmail = req.query.userEmail;
+    var url = API_EMAIL_ENDPOINT + '?' + API_SET_EMAIL_SUFFIX + '&email_user=' + userEmail;
+    var options = {
+          url: url,
+          headers: {
+              Cookie: 'PHPSESSID=' + credentials
+          }
+    };
+    
+    request(options, function (error, response, body) {
+        var cookiesHeader = {};
+        if (!error && response.statusCode === 200) {
+            response.headers['set-cookie'].map(function(cookieArray){
+                cookieArray.split(';').map(function(cookie){
+                    var cookieData = cookie.split('=');
+                    cookiesHeader[cookieData[0]] = cookieData[1] || '';
+                });
+            });
+
+            res.send({cookies: cookiesHeader, emailData: JSON.parse(body)});
+        }else{
+          res.status(500).send(error);
+        }
+    });
+});
+
+app.get('/email/checkemail', function(req, res){
+    var credentials = req.query.token;
+    var url = API_EMAIL_ENDPOINT + '?' + API_CHECK_EMAIL_SUFFIX;
+    var options = {
+          url: url,
+          headers: {
+              Cookie: 'PHPSESSID=' + credentials
+          }
+    };
+    
+    request(options, function (error, response, body) {
+        var cookiesHeader = {};
+        if (!error && response.statusCode === 200) {
+            response.headers['set-cookie'].map(function(cookieArray){
+                cookieArray.split(';').map(function(cookie){
+                    var cookieData = cookie.split('=');
+                    cookiesHeader[cookieData[0]] = cookieData[1] || '';
+                });
+            });
+
+            res.send({cookies: cookiesHeader, emailList: JSON.parse(body)});
+        }else{
+            console.log('error');
+            res.status(500).send(error);
+        }
+    });
+});
+
+app.get('/email/getemail', function(req, res){
+    var credentials = req.query.token;
+    var emailId = req.query.emailId;
+    var url = API_EMAIL_ENDPOINT + '?' + API_GET_EMAIL_SUFFIX + '&email_id=' + emailId;
+    var options = {
+          url: url,
+          headers: {
+              Cookie: 'PHPSESSID=' + credentials
+          }
+    };
+    
+    request(options, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        res.send(JSON.parse(body));
+      }else{
+        res.status(500).send(error);
+      }
+    });
+});
+
+app.get('/', function(req, res) {
+    res.send('Corbel-js express server');
+});
+
+app.listen(process.env.PORT || 3000);
+console.log('server started');
+
+module.exports = app;
