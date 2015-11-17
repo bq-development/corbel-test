75328f2bb9aa7629cdbf53a371ab64008a0eef2d
ricardo <ricardo.martinez@bq.com>
2015-11-17 11:41:27 +0100

    create and get notification tests added
    Change-Id: I792e681afb3615f77d1152833931157fe7dc743b

---
+++ src/common/notifications/notificationsCommon.js
@@ -0,0 +1,57 @@
+//@exclude
+'use strict';
+/*globals corbel */
+//@endexclude
+
+function createMultipleNotifications (driver, amount) {
+    var promises = [];
+    for (var count = 1; count < amount; count++) {
+        var promise = createNotification(driver, count)
+            .should.be.eventually.fulfilled;
+        promises.push(promise);
+    }
+    return Promise.all(promises);
+}
+
+function createNotification(driver, number) {
+    return driver.notifications.notification().create(getRandomNotification(number));
+}
+
+function getRandomNotification(number) {
+    number = number || 0;
+    return {
+        id: 'mail_notification_' + number + '_' + Date.now(),
+        type: 'mail',
+        sender: 'me',
+        text: 'text',
+        title: 'subject'
+    };
+}
+
+function deleteNotificationsList(driver, notificationList) {
+    var promises = [];
+    notificationList.forEach(function(id) {
+        var promise = driver.notifications.notification(id)
+            .delete()
+            .should.be.eventually.fulfilled
+            .then(function(){
+                return driver.notifications.notification(id)
+                    .get()
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            });
+        promises.push(promise);
+    });
+    return Promise.all(promises);
+}
+
+
+module.exports = {
+    createMultipleNotifications: createMultipleNotifications,
+    createNotification: createNotification,
+    getRandomNotification: getRandomNotification,
+    deleteNotificationsList: deleteNotificationsList
+};
