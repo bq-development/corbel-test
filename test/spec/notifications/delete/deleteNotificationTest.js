da5c4ac4885160c575bb735c009e0034d0bde3ba
ricardo <ricardo.martinez@bq.com>
2015-11-17 12:00:25 +0100

    deleteNotificacion test added
    Change-Id: Ic59d6a3433c682c84a0b91c9a87636659652661d

---
+++ test/spec/notifications/delete/deleteNotificationTest.js
@@ -0,0 +1,37 @@
+describe('In NOTIFICATIONS module', function() {
+
+    describe('when testing deletion', function() {
+        var corbelDriver;
+        var notificationId;
+
+        before(function() {
+            corbelDriver = corbelTest.drivers['ADMIN_USER'].clone();
+        });
+
+        beforeEach(function(done) {
+            corbelTest.common.notifications.createNotification(corbelDriver)
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                notificationId = id;
+            })
+            .should.notify(done);
+        });
+
+        afterEach(function(done) {
+            corbelDriver.notifications.notification(notificationId)
+                .get()
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('a notification can be deleted', function(done) {
+            corbelDriver.notifications.notification(notificationId)
+                .delete()
+            .should.be.eventually.fulfilled.and.notify(done);
+        });
+    });
+});
