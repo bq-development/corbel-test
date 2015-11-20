4defa22d508107ab8cb25a99d62a7eb5445cf91e
ricardo <ricardo.martinez@bq.com>
2015-11-20 10:19:17 +0100

    [Patch 2]new aggregation param tested
    Change-Id: Ib5f4897106270164a752cc221ff57720eecd10f6

--- test/spec/resources/resmi/relation/aggregation/relationAggregationTest.js
+++ test/spec/resources/resmi/relation/aggregation/relationAggregationTest.js
@@ -82,6 +82,98 @@ describe('In RESOURCES module ', function() {
                 .should.notify(done);
             });
 
+            it('in histogram aggregation case, the number of occurrences of each value in a field are returned',
+                    function(done) {
+                var params = {
+                    aggregation: {
+                        '$histogram': 'distinctField'
+                    }
+                };
+
+                corbelDriver.resources.relation(COLLECTION_A, idResourceInA, COLLECTION_B)
+                .get(null, params)
+                .should.be.eventually.fulfilled
+                .then(function(response) {
+                    expect(response).to.have.deep.property('data.length', 2);
+                    response.data.map(function(resource){
+                        expect(resource).to.have.property('id').and.to.be.within(0,1);
+                        expect(resource).to.have.property('count', amount/2);
+                    });
+                })
+                .should.be.eventually.fulfilled.and.notify(done);
+            });
+
+            it('in histogram aggregation case, if the field does not exists, the count is the number of resources',
+                    function(done) {
+                var params = {
+                    aggregation: {
+                        '$histogram': 'nonExist'
+                    }
+                };
+
+                corbelDriver.resources.relation(COLLECTION_A, idResourceInA, COLLECTION_B)
+                .get(null, params)
+                .should.be.eventually.fulfilled
+                .then(function(response) {
+                    expect(response).to.have.deep.property('data.length', 1);
+                    response.data.map(function(resource){
+                        expect(resource).not.to.have.property('id');
+                        expect(resource).to.have.property('count', amount);
+                    });
+                })
+                .should.be.eventually.fulfilled.and.notify(done);
+            });
+
+            it('in histogram aggregation case, the number of occurrences in a field that match the query are returned',
+                    function(done) {
+                var params = {
+                    aggregation: {
+                        '$histogram': 'intField'
+                    },
+                    query: [{
+                        '$lt': {
+                            intField: 1000
+                        }
+                    }]
+                };
+
+                corbelDriver.resources.relation(COLLECTION_A, idResourceInA, COLLECTION_B)
+                .get(null, params)
+                .should.be.eventually.fulfilled
+                .then(function(response) {
+                    expect(response).to.have.property('data').and.to.be.an('array');
+                    response.data.map(function(resource){
+                        expect(resource).to.have.property('id').and.to.be.below(1000);
+                        expect(resource).to.have.property('count');
+                    });
+                })
+                .should.be.eventually.fulfilled.and.notify(done);
+            });
+
+            it('in histogram aggregation case, the number of occurrences that match the pagination are returned',
+                    function(done) {
+                var params = {
+                    aggregation: {
+                        '$histogram': 'stringField'
+                    },
+                    pagination: {
+                        pageSize: 5
+                    }
+                };
+
+                corbelDriver.resources.relation(COLLECTION_A, idResourceInA, COLLECTION_B)
+                .get(null, params)
+                .should.be.eventually.fulfilled
+                .then(function(response) {
+                    expect(response).to.have.deep.property('data.length', 5);
+                    response.data.map(function(resource){
+                        expect(resource).to.have.property('id').and.to.contain('stringContent');
+                        expect(resource).to.have.property('count');
+                    });
+                })
+                .should.be.eventually.fulfilled.and.notify(done);
+            });
+
             it('with average aggregation, expected average value is returned.', function(done) {
                 var params = {
                     aggregation: {
