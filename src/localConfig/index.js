636a27f198964cfeff916a3272e004170718d0be
vipereybq <victor.perezrey@bq.com>
2015-11-24 18:46:28 +0100

    [Patch 5] SILKROAD-2168 - grep, environment and local services through console & browser args
    Change-Id: I12e09407935817bad4cedb986c901a14f992d48f

---
+++ src/localConfig/index.js
@@ -0,0 +1,57 @@
+var _ = require('lodash');
+
+var LocalConfig = function Local() {
+    this.LOCAL_STORAGE_KEY = 'config';
+    this.db = this.load() || {};
+};
+
+LocalConfig.prototype = {
+
+    setOriginalURL: function(url) {
+        this.db.URL_BASE_ORIGINAL = url;
+        this.save();
+    },
+
+    getOriginalUrl: function() {
+        return this.db.URL_BASE_ORIGINAL;
+    },
+
+    setEnvironment: function(env) {
+        this.db.ENV = env;
+        this.save();
+    },
+
+    getEnvironment: function() {
+        return this.db.ENV;
+    },
+
+    setLocalServices: function(services) {
+        this.db.localServices = services;
+        this.save();
+    },
+
+    getLocalServices: function() {
+        return this.db.localServices;
+    },
+
+    removeLocalService: function(service) {
+        this.db.localServices = _.without(this.db.localServices, service);
+        this.save();
+    },
+
+    addLocalService: function(service) {
+        this.db.localServices = this.db.localServices || [];
+        this.db.localServices.push(service);
+        this.save();
+    },
+
+    save: function() {
+        window.localStorage.setItem(this.LOCAL_STORAGE_KEY, JSON.stringify(this.db));
+    },
+
+    load: function() {
+        return JSON.parse(window.localStorage.getItem(this.LOCAL_STORAGE_KEY));
+    }
+};
+
+exports.LocalConfig = new LocalConfig();
--- src/main.js
+++ src/main.js
@@ -2,21 +2,76 @@ var _ = require('lodash');
 var common = require('./common');
 var fixtures = require('./fixtures');
 var config = require('../.tmp/config.js');
+var $ = require('jquery');
+var localConfig = require('./localConfig').LocalConfig;
+var Sidebar = require('./sidebar').Sidebar;
+var sidebar = new Sidebar(localConfig);
+
 var corbelTest = {};
+
+var initEnvironment = function(config, process, karma, local) {
+    if (local.getEnvironment() !== undefined) {
+        return local.getEnvironment();
+    } else if (karma.config.env && karma.config.env !== undefined) {
+        return karma.config.env;
+    } else {
+        return process.env.NODE_ENV ? process.env.NODE_ENV : config.ENV;
+    }
+};
+
+var initLocalServices = function(karma, local) {
+    if (local.getLocalServices() !== undefined) {
+        return local.getLocalServices();
+    } else {
+        return (karma.config.localServices) ?
+            karma.config.localServices.match(/(^\[(((.)+(,)?)+)\]$)/)[2].split(',') : [];
+    }
+};
+
+var setupGrep = function(karma) {
+    if (karma.config.grep) {
+        var debugHref = $(parent.document).find('a.btn-debug').attr('href');
+        $(parent.document).find('a.btn-debug').attr('href', debugHref + '?grep=' + karma.config.grep);
+    }
+};
+
+var setupBrowser = function(karma, localServices, environment) {
+    if (window.chrome) {
+        setupGrep(karma);
+        sidebar.setup(localServices, environment);
+    }
+};
+
+var saveLocalConfig = function(environment, localServices) {
+    localConfig.setEnvironment(environment);
+    localConfig.setLocalServices(localServices);
+};
 
 corbelTest.CONFIG = _.cloneDeep(config);
 
-//Initialize correct urlBase
-var environment = process.env.NODE_ENV ? process.env.NODE_ENV : config.ENV;
+var karma = window.__karma__;
+var environment = initEnvironment(config, process, karma, localConfig);
+var localServices = initLocalServices(karma, localConfig);
 corbelTest.CONFIG.COMMON.urlBase = config.COMMON.urlBase.replace('{{ENV}}', environment);
+
+saveLocalConfig(environment, localServices);
+setupBrowser(karma, localServices, environment);
 
 corbelTest.getConfig = function(clientName) {
     var data = {
-        urlBase: corbelTest.CONFIG.COMMON.urlBase
+        urlBase: corbelTest.CONFIG.COMMON.urlBase,
+        localServices: localServices
     };
+    _.each(Object.keys(corbelTest.CONFIG.COMMON.endpoints), function(service) {
+        if (_.contains(localServices, service)) {
+            data[service + 'Endpoint'] = corbelTest.CONFIG.COMMON.endpoints[service];
+        }
+    });
+
     return _.extend(data, corbelTest.CONFIG[clientName]);
 };
 
+corbelTest.localConfig = localConfig;
 corbelTest.common = common;
 corbelTest.fixtures = fixtures;
 corbelTest.drivers = common.clients.drivers;
