27ff94f69e364194fdb10467660720a70ebfb04f
samuelmartingc <samuelmartingc@gmail.com>
2015-11-25 09:09:53 +0100

    tested blur effect
    Change-Id: If7ed0b6663d729edc3be16fb9efd12b7622deb7f

--- test/spec/resources/rem-image/image-base64.js
+++ test/spec/resources/rem-image/image-base64.js
@@ -1,19 +1,19 @@
 describe('In RESOURCES module', function() {
-	describe('In RESTOR module, while using REM-IMAGEs plugin', function() {
+    describe('In RESTOR module, while using REM-IMAGEs plugin', function() {
 
-		var corbelDriver;
+        var corbelDriver;
         var FOLDER_NAME = 'test:Restor';
 
         before(function() {
             corbelDriver = corbelTest.drivers['ADMIN_CLIENT'].clone();
         });
 
-        describe('When performing image:operations to base64 type images', function() {
+        describe('when performing image:operations to base64 type images', function() {
             if (window.chrome) {
-                var TEST_IMAGE = 
-                    'Qk2eAAAAAAAAAHoAAABsAAAAAwAAAAMAAAABABgAAAAAACQAAAATCwAAEwsAAAAAAAAAAAAAQkdScwAAAAAAAA'+
-                    'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAD//wD/AP//'+
-                    'AAAAAABmZmYA/wAAAH8AAAAAAP9/AAAA//8AAAA=';
+                var TEST_IMAGE =
+                'Qk2eAAAAAAAAAHoAAABsAAAAAwAAAAMAAAABABgAAAAAACQAAAATCwAAEwsAAAAAAAAAAAAAQkdScwAAAAAAAA'+
+                'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAD//wD/AP//'+
+                'AAAAAABmZmYA/wAAAH8AAAAAAP9/AAAA//8AAAA=';
                 var TEST_IMAGE_SIZE = 158;
                 var FILENAME;
 
@@ -32,7 +32,7 @@ describe('In RESOURCES module', function() {
                             dataType:'image/bmp',
                             responseType: 'arraybuffer',
                             customQueryParams: {
-                                    'image:operations': operationQuery
+                                'image:operations': operationQuery
                             },
                         }
                     )
@@ -43,7 +43,7 @@ describe('In RESOURCES module', function() {
                     FILENAME = 'TestImage_1_' + Date.now();
 
                     corbelDriver.resources.resource(FOLDER_NAME, FILENAME).update(
-                        TEST_IMAGE, 
+                        TEST_IMAGE,
                         {
                             dataType:'image/bmp',
                             customQueryParams: {
@@ -69,17 +69,17 @@ describe('In RESOURCES module', function() {
                     .should.notify(done);
                 });
 
-                it('Success changing image\'s format (bmp to gif)', function(done) {
-                    var TEST_FORMAT = 
-                        'R0lGODlhAwADAPMAAP8AAAAAf///AGZmZgD/AH8AAAD///8A/wAA/wAAAAAAAAAAAAAAAAAAAAAAAAAAA'+
-                        'CH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sHZ2FtbWE9MAAsAAAAAAMAAwAABAcQBDFIMQdFADs=';
+                it('success changing image s format (bmp to gif)', function(done) {
+                    var TEST_FORMAT =
+                    'R0lGODlhAwADAPMAAP8AAAAAf///AGZmZgD/AH8AAAD///8A/wAA/wAAAAAAAAAAAAAAAAAAAAAAAAAAA'+
+                    'CH5BAAAAAAAIf8LSW1hZ2VNYWdpY2sHZ2FtbWE9MAAsAAAAAAMAAwAABAcQBDFIMQdFADs=';
 
                     return corbelDriver.resources.resource(FOLDER_NAME, FILENAME).get(
                         {
                             dataType:'image/bmp',
                             responseType: 'arraybuffer',
                             customQueryParams: {
-                                    'image:format': 'gif'
+                                'image:format': 'gif'
                             },
                         }
                     )
@@ -90,11 +90,11 @@ describe('In RESOURCES module', function() {
                     .should.notify(done);
                 });
 
-                it('crop', function(done) {
-                    var TEST_CROP = 
-                        'Qk2aAAAAAAAAAIoAAAB8AAAAAgAAAAIAAAABABgAAAAAABAAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
-                        'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
-                        'AEAAAAAAAAAAAAAAAAAAAAZmZmAP8AAAAAAP9/AAAAAA==';
+                it('applying crop operation', function(done) {
+                    var TEST_CROP =
+                    'Qk2aAAAAAAAAAIoAAAB8AAAAAgAAAAIAAAABABgAAAAAABAAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
+                    'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
+                    'AEAAAAAAAAAAAAAAAAAAAAZmZmAP8AAAAAAP9/AAAAAA==';
 
                     getImageModified('crop=(0,0,2,2)')
                     .then(function(img) {
@@ -103,11 +103,27 @@ describe('In RESOURCES module', function() {
                     .should.notify(done);
                 });
 
-                it('resizeWidth', function(done) {
-                    var TEST_RESIZE_WIDTH = 
-                        'Qk2aAAAAAAAAAIoAAAB8AAAAAgAAAAIAAAABABgAAAAAABAAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
-                        'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
-                        'AEAAAAAAAAAAAAAAAAAAAAv7ZTox5bAAAsPHEQl3kAAA==';
+                it('applying blur operation', function(done) {
+                    var TEST_BLUR =
+                    'Qk2uAAAAAAAAAIoAAAB8AAAAAwAAAAMAAAABABgAAAAAAC'+
+                    'QAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
+                    'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAA'+
+                    'AAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAA'+
+                    'AAAAAAAEAAAAAAAAAAAAAAAAAAAAyatKvVxetBZ'+
+                    'PAAAAc3JpZmRmVlRwAAAALyuVLlx9GpWaAAAA';
+
+                    getImageModified('blur=(0.1 , 1.2)')
+                    .then(function(img) {
+                        compareImages(img.data, TEST_BLUR);
+                    })
+                    .should.notify(done);
+                });
+
+                it('applying resizeWidth operation', function(done) {
+                    var TEST_RESIZE_WIDTH =
+                    'Qk2aAAAAAAAAAIoAAAB8AAAAAgAAAAIAAAABABgAAAAAABAAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
+                    'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
+                    'AEAAAAAAAAAAAAAAAAAAAAv7ZTox5bAAAsPHEQl3kAAA==';
 
                     getImageModified('resizeWidth=2')
                     .then(function(img) {
@@ -116,11 +132,11 @@ describe('In RESOURCES module', function() {
                     .should.notify(done);
                 });
 
-                it('resizeHeight', function(done) {
-                    var TEST_RESIZE_HEIGHT = 
-                        'Qk2aAAAAAAAAAIoAAAB8AAAAAgAAAAIAAAABABgAAAAAABAAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
-                        'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
-                        'AEAAAAAAAAAAAAAAAAAAAAv7ZTox5bAAAsPHEQl3kAAA==';
+                it('applying resizeHeight operation', function(done) {
+                    var TEST_RESIZE_HEIGHT =
+                    'Qk2aAAAAAAAAAIoAAAB8AAAAAgAAAAIAAAABABgAAAAAABAAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
+                    'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
+                    'AEAAAAAAAAAAAAAAAAAAAAv7ZTox5bAAAsPHEQl3kAAA==';
 
                     getImageModified('resizeHeight=2')
                     .then(function(img) {
@@ -129,11 +145,11 @@ describe('In RESOURCES module', function() {
                     .should.notify(done);
                 });
 
-                it('resizeAndFill', function(done) {
-                    var TEST_RESIZE_AND_FILL = 
-                        'Qk2uAAAAAAAAAIoAAAB8AAAAAwAAAAMAAAABABgAAAAAACQAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
-                        'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
-                        'AEAAAAAAAAAAAAAAAAAAAA//8A/wD//wAAAAAAZmZmAP8AAAB/AAAAAAD/fwAAAP//AAAA';
+                it('applying resizeAndFill operation', function(done) {
+                    var TEST_RESIZE_AND_FILL =
+                    'Qk2uAAAAAAAAAIoAAAB8AAAAAwAAAAMAAAABABgAAAAAACQAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
+                    'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
+                    'AEAAAAAAAAAAAAAAAAAAAA//8A/wD//wAAAAAAZmZmAP8AAAB/AAAAAAD/fwAAAP//AAAA';
 
                     getImageModified('resizeAndFill=(3, 0000FF)')
                     .then(function(img) {
@@ -142,11 +158,11 @@ describe('In RESOURCES module', function() {
                     .should.notify(done);
                 });
 
-                it('cropFromCenter', function(done) {
-                    var TEST_CROP_FROM_CENTER = 
-                        'Qk2OAAAAAAAAAIoAAAB8AAAAAQAAAAEAAAABABgAAAAAAAQAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
-                        'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
-                        'AEAAAAAAAAAAAAAAAAAAAAAP8AAA==';
+                it('applying cropFromCenter operation', function(done) {
+                    var TEST_CROP_FROM_CENTER =
+                    'Qk2OAAAAAAAAAIoAAAB8AAAAAQAAAAEAAAABABgAAAAAAAQAAAATCwAAEwsAAAAAAAAAAAAAAAD/AAD/A'+
+                    'AD/AAAAAAAA/0JHUnMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA0M7Mvr0fhQz8KV4UAAAAAAAAAAAAAAA'+
+                    'AEAAAAAAAAAAAAAAAAAAAAAP8AAA==';
 
                     getImageModified('cropFromCenter=(1, 1)')
                     .then(function(img) {
