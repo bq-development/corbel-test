5cf3a30fce29b18fa5081cad4cad714da5e19552
ricardo <ricardo.martinez@bq.com>
2015-11-25 09:36:28 +0100

    group tests bugfixed
    Change-Id: Ie95d693834b5299bef35f90cdb271d0db3886feb

--- test/spec/iam/group/crudGroupErrorTest.js
+++ test/spec/iam/group/crudGroupErrorTest.js
@@ -37,7 +37,7 @@ describe('In IAM module', function() {
                 });
 
             Promise.all(promises)
-            .should.be.eventually.fulfilled.and.notify(done);
+            .should.notify(done);
         });
 
         afterEach(function(done){
@@ -58,7 +58,7 @@ describe('In IAM module', function() {
                     promises.push(promise);
                 });
             Promise.all(promises)
-            .should.be.eventually.fulfilled.and.notify(done);
+            .should.notify(done);
         });
 
         it('an error [401] is returned when tryinging to create a group without propper permissions', function(done) {
