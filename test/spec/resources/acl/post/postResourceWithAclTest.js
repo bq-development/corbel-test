fc717f6c149819b9d9e3df7ac184c82ed48d6091
ricardo <ricardo.martinez@bq.com>
2015-11-26 13:15:01 +0100

    postResourceWithAcl tests added
    Change-Id: Iceedea7fc9c115fcf728e35ac1218cf3156da07e

---
+++ test/spec/resources/acl/post/postResourceWithAclTest.js
@@ -0,0 +1,84 @@
+describe('In RESOURCES module', function() {
+
+    describe('In ACL module', function() {
+
+        describe('while testing post resources', function() {
+            var corbelDriver;
+            var corbelRootDriver;
+            var COLLECTION_NAME = 'test:testAcl';
+            var user;
+            var resourceId;
+            var random;
+
+            before(function(){
+                corbelRootDriver = corbelTest.drivers['ADMIN_USER'].clone();
+            });
+
+            beforeEach(function(done) {
+                corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
+                random = Date.now();
+
+                corbelTest.common.iam.createUsers(corbelDriver, 1)
+                .should.be.eventually.fulfilled
+                .then(function(createdUser) {
+                    user = createdUser[0];
+
+                    return corbelTest.common.clients.loginUser(corbelDriver, user.username, user.password)
+                    .should.be.eventually.fulfilled;
+                })
+                .should.notify(done);
+            });
+
+            afterEach(function(done) {
+                corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                    .delete()
+                .should.be.eventually.fulfilled
+                .then(function() {
+                    return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                    .get()
+                    .should.be.eventually.rejected;
+                })
+                .then(function(e) {
+                    expect(e).to.have.property('status', 404);
+                    expect(e).to.have.deep.property('data.error', 'not_found');
+
+                    return corbelRootDriver.iam.user(user.id)
+                    .delete()
+                    .should.be.eventually.fulfilled;
+                })
+                .then(function() {
+                    return corbelRootDriver.iam.user(user.id)
+                    .get()
+                    .should.be.eventually.rejected;
+                })
+                .then(function(e) {
+                    expect(e).to.have.property('status', 404);
+                    expect(e).to.have.deep.property('data.error', 'not_found');
+                })
+                .should.notify(done);
+            });
+
+            it('a resource can be created with ACL', function(done) {
+                var TEST_OBJECT = {
+                    test: 'test' + random,
+                    test2: 'test2' + random
+                };
+
+                corbelDriver.resources.collection(COLLECTION_NAME)
+                    .add(TEST_OBJECT)
+                .should.be.eventually.fulfilled
+                .then(function(id) {
+                    resourceId = id;
+
+                    return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                        .get()
+                    .should.be.eventually.fulfilled;
+                })
+                .then(function(response) {
+                    expect(response).to.have.deep.property('data._acl.user:' + user.id + '.permission','ADMIN');
+                })
+                .should.notify(done);
+            });
+        });
+    });
+});
