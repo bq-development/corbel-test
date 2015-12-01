2f19bc7ee7a775dc8242eedd0309dd4fcb9ec96b
laurasada <laura.sada@bq.com>
2015-12-01 15:38:35 +0100

    migrated implicit scopes
    Change-Id: I4c2976445b0cbfc23a8b50fb8c90de79d9f70288

---
+++ test/spec/iam/token/implicitScopesLoginTest.js
@@ -0,0 +1,89 @@
+describe('In IAM module, while using implicit scopes', function() {
+
+    it('when request client login without scopes, an access token is returned', function(done) {
+        var claimsAdmin = _.cloneDeep(corbelTest.CONFIG['ADMIN_CLIENT']);
+        var corbelNewDriver = corbel.getDriver({
+            'clientId': claimsAdmin.clientId,
+            'clientSecret': claimsAdmin.clientSecret,
+            'urlBase': corbelTest.CONFIG.COMMON.urlBase,
+            'scopes': []
+        });
+
+        corbelNewDriver.iam.token()
+            .create()
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+
+                expect(response).to.have.deep.property('data.accessToken');
+                return corbelNewDriver.iam.user('fooid')
+                    .get()
+                    .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.id', 'fooid');
+            })
+            .should.notify(done);
+    });
+
+    it('when request user login with empty scopes, an access token is returned', function(done) {
+        var claimsDefaultUserDriver = _.cloneDeep(corbelTest.CONFIG['DEFAULT_USER']);
+        var corbelDefaultDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+
+        corbelDefaultDriver.iam
+            .token()
+            .create({
+                claims: {
+                    'basic_auth.username': claimsDefaultUserDriver.username,
+                    'basic_auth.password': claimsDefaultUserDriver.password,
+                    'scopes': ''
+                }
+            })
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.accessToken');
+                return corbelDefaultDriver.iam.user('me')
+                    .get()
+                    .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.username', claimsDefaultUserDriver.username);
+            })
+            .should.notify(done);
+    });
+
+    it('when request an access token through a domain request without scopes', function(done) {
+        var version = _.cloneDeep(corbelTest.CONFIG['VERSION']);
+        var aud = 'http://iam.bqws.io';
+        var jwtAlgorithm = 'HS256';
+        var requestDomain = 'silkroad-qa';
+        var clientSecret = '176ffba7f85493f781b5f31f7bf43a1133d32fec3aadf207cd2bc48393a19e17';
+        var tokenValidation = /^.+\..+\..+$/;
+        var claims = {
+            'iss': 'f6a28cc7',
+            'request_domain': requestDomain,
+            'aud': aud,
+            'scope': [],
+            'version': version
+        };
+        var corbelNewDriver = corbel.getDriver({
+            domain: corbelTest.CONFIG.DOMAIN,
+            urlBase: corbelTest.CONFIG.COMMON.urlBase
+        });
+
+        corbelNewDriver.iam
+            .token()
+            .create({
+                jwt: corbel.jwt.generate(
+                    claims,
+                    clientSecret,
+                    jwtAlgorithm
+                )
+            })
+            .should.be.eventually.fulfilled
+            .then(function(token) {
+                expect(token).to.have.deep.property('data.accessToken').and.to.match(tokenValidation);
+
+            })
+            .should.notify(done);
+    });
+});
