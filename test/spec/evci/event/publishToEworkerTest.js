d8c4318a09f953a343776bc417d445073d18503d
ricardo <ricardo.martinez@bq.com>
2015-12-04 12:34:01 +0100

    publishToEworkerTest improved
    Change-Id: I04e0ef17f8f54999140b4b2a655060fa4e8b4da7

--- test/spec/evci/event/publishToEworkerTest.js
+++ test/spec/evci/event/publishToEworkerTest.js
@@ -5,7 +5,7 @@ describe('In EVCI module', function() {
       corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
     });
 
-    it('should create/put a resource after an event is sent to the proper worker', function(done) {
+    it('a resource should be created/put after an event is sent to the proper worker', function(done) {
         var MAX_RETRY = 30;
         var RETRY_PERIOD = 2;
 
@@ -16,21 +16,22 @@ describe('In EVCI module', function() {
             message: 'hello evci'
         };
 
-        corbelDriver.evci.event(eventType).publish(event)
+        corbelDriver.evci.event(eventType)
+            .publish(event)
         .should.be.eventually.fulfilled
         .then(function() {
             return corbelTest.common.utils.retry(function() {
                 return corbelDriver.resources.resource(eventType, event.id)
                 .get()
                 .should.be.eventually.fulfilled;
-            }, MAX_RETRY, RETRY_PERIOD).
-            should.be.eventually.fulfilled;
+            }, MAX_RETRY, RETRY_PERIOD)
+            .should.be.eventually.fulfilled;
         })
         .then(function(response) {
             expect(response).to.have.deep.property('data.id', event.id);
             expect(response).to.have.deep.property('data.timestamp', event.timestamp);
             expect(response).to.have.deep.property('data.message', event.message);
         })
-        .should.be.eventually.fulfilled.and.notify(done);    
+        .should.notify(done);    
     });           
 });
