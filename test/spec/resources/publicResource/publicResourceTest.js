be153f69cf5acf655da011a27d38d1b33079d7c8
Cristian del Cerro <cristian.delcerro@bq.com>
2015-12-07 16:22:15 +0100

    CRUD tests public resources
    Change-Id: I5f08268f4f00b7af89ac7333c7d858a8b50f66d3

--- test/spec/resources/publicResource/publicResourceTest.js
+++ test/spec/resources/publicResource/publicResourceTest.js
@@ -1,61 +1,70 @@
 describe('In RESOURCES module', function() {
     var corbelRootDriver;
-    var corbelTestDriver;
+    var corbelDriver;
 
-    before(function() {
+    var domainId;
+    var publicResourceTestCollection = 'test:PublicResource';
+    var random;
+    var currentResourcesEndpoint;
+
+    beforeEach(function(done) {
+        var domain;
+        random = Date.now();
+
         corbelRootDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+
+        currentResourcesEndpoint = corbelTest.getConfig().localServices.indexOf('resources') !== -1 ? 
+            corbelTest.getConfig().resourcesEndpoint :
+            corbelTest.getConfig().urlBase.replace('{{module}}', 'resources');
+
+        var scopeId = 'scopeId-' + random;
+        var audience = 'http://resources.bqws.io';
+        var rules = [{
+            mediaTypes: ['application/json'],
+            methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            type: 'http_access',
+            uri: 'resource/' + publicResourceTestCollection + '(/.*)?' 
+        }];
+
+        var publicScope = {
+            id: scopeId,
+            audience: audience ,
+            rules: rules
+        };
+
+        var publicScopes = [scopeId];
+        var scopes = ['silkroad-qa:resources', scopeId];
+
+        domain = corbelTest.common.iam.getDomain(undefined, undefined, undefined, scopes, publicScopes);
+          
+        corbelRootDriver.iam.scope()
+        .create(publicScope)
+        .should.be.eventually.fulfilled
+        .then(function() {
+            return corbelRootDriver.iam.domain()
+            .create(domain) 
+            .should.be.eventually.fulfilled;   
+        })
+        .then(function(id) {
+            domainId = id;
+        })
+        .should.notify(done);
     });
 
     describe('if a resource is public', function(done) {
-        var domain;
-        var domainId;
-        var publicResourceTestCollection = 'test:PublicResource';
-        var idResource;
-        var confCreatedClient;
+        var resourceId;
 
-        before(function(done) {
-            var scopeId = 'scopeId-' + Date.now();
-            var audience = 'http://resources.bqws.io';
-            var rules = [{
-                mediaTypes: ['application/json'],
-                methods: ['GET'],
-                type: 'http_access',
-                uri: 'resource/' + publicResourceTestCollection + '(/.*)?' 
-            }];
-
-            var publicScope = {
-                id: scopeId,
-                audience: audience ,
-                rules: rules
+        beforeEach(function(done) {
+            var client = {
+                name: 'testClient_' + random,
+                signatureAlgorithm: 'HS256',
+                domain: domainId,
+                scopes: ['silkroad-qa:resources']
             };
 
-            var publicScopes = [scopeId];
-            var scopes = ['silkroad-qa:resources', scopeId];
-
-            domain = corbelTest.common.iam.getDomain(undefined, undefined, undefined, scopes, publicScopes);
-          
-            corbelRootDriver.iam.scope()
-            .create(publicScope)
+            corbelRootDriver.iam.client(domainId)
+            .create(client)
             .should.be.eventually.fulfilled
-            .then(function() {
-                return corbelRootDriver.iam.domain()
-                .create(domain) 
-                .should.be.eventually.fulfilled;   
-            })
-            .then(function(id) {
-                domainId = id;
-
-                var client = {
-                    name: 'testClient_' + Date.now(),
-                    signatureAlgorithm: 'HS256',
-                    domain: domainId,
-                    scopes: ['silkroad-qa:resources']
-                };
-
-                return corbelRootDriver.iam.client(domainId)
-                    .create(client)
-                    .should.be.eventually.fulfilled;
-            })
             .then(function(clientId) {
                 return corbelRootDriver.iam.client(domainId, clientId)
                     .get()
@@ -63,42 +72,40 @@ describe('In RESOURCES module', function() {
             })
             .then(function(response) {
                 var createdClient = response.data;
-                confCreatedClient = corbelTest.getConfig();
+                var confCreatedClient = corbelTest.getConfig();
                 confCreatedClient.clientId = createdClient.id;
                 confCreatedClient.clientSecret = createdClient.key;
                 confCreatedClient.scopes = createdClient.scopes.join(' ');
-                corbelTestDriver = corbel.getDriver(confCreatedClient);
+                corbelDriver = corbel.getDriver(confCreatedClient);
 
-                return corbelTestDriver.iam.token().create();
+                return corbelDriver.iam.token()
+                    .create();
             })
             .then(function() {
-                var testResource = {
+                var resourceTest = {
                     testField : 'testContent'
                 };
 
-                return corbelTestDriver.resources.collection(publicResourceTestCollection)
-                .add(testResource)
-                .should.be.eventually.fulfilled;
+                return corbelDriver.resources.collection(publicResourceTestCollection)
+                    .add(resourceTest)
+                    .should.be.eventually.fulfilled;
             })
             .then(function(id) {
-                idResource = id;
+                resourceId = id;
             })
             .should.notify(done);
         });
 
-        after(function(done) {
-            corbelTestDriver.resources.resource(publicResourceTestCollection, idResource)
+        afterEach(function(done) {
+            corbelDriver.resources.resource(publicResourceTestCollection, resourceId)
             .delete()
             .should.be.eventually.fulfilled.and.should.notify(done);
         });
 
-        it('that can be consulted without token.', function(done) {
-            var currentResourcesEndpoint = confCreatedClient.localServices.indexOf('resources') !== -1 ? 
-            confCreatedClient.resourcesEndpoint : confCreatedClient.urlBase.replace('{{module}}', 'resources');
-
+        it('that can be consulted without token', function(done) {
             corbel.request.send({
                 url: currentResourcesEndpoint + domainId + '/resource/' + 
-                    publicResourceTestCollection + '/' +idResource,
+                    publicResourceTestCollection + '/' +resourceId,
                 method: corbel.request.method.GET,
                 headers: {
                     'Accept': 'application/json'
@@ -107,7 +114,118 @@ describe('In RESOURCES module', function() {
             })
             .should.be.eventually.fulfilled
             .then(function(response) {
-                expect(response).to.have.deep.property('data.id', idResource);
+                expect(response).to.have.deep.property('data.id', resourceId);
+            })
+            .should.notify(done);
+        });
+
+        it('that can be update without token', function(done) {
+            corbel.request.send({
+                url: currentResourcesEndpoint + domainId + '/resource/' + 
+                    publicResourceTestCollection + '/' +resourceId,
+                method: corbel.request.method.PUT,
+                headers: {
+                    'Accept': 'application/json'
+                }, 
+                data: {
+                    testField: 'testContentUpdated'
+                }
+
+            })
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbel.request.send({
+                    url: currentResourcesEndpoint + domainId + '/resource/' + 
+                        publicResourceTestCollection + '/' +resourceId,
+                    method: corbel.request.method.GET,
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+
+                })
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.id', resourceId);
+                expect(response).to.have.deep.property('data.testField', 'testContentUpdated');
+            })
+            .should.notify(done);
+        });
+
+        it('that can be deleted without token', function(done) {
+            corbel.request.send({
+                url: currentResourcesEndpoint + domainId + '/resource/' + 
+                    publicResourceTestCollection + '/' +resourceId,
+                method: corbel.request.method.DELETE,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbel.request.send({
+                    url: currentResourcesEndpoint + domainId + '/resource/' + 
+                        publicResourceTestCollection + '/' +resourceId,
+                    method: corbel.request.method.GET,
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+
+                })
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+    });
+
+    describe('if a resource public is added', function(done) {
+        var resourceId;
+
+        after(function(done) {
+            corbel.request.send({
+                url: currentResourcesEndpoint + domainId + '/resource/' + 
+                    publicResourceTestCollection + '/' +resourceId,
+                method: corbel.request.method.DELETE,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            })
+            .should.be.eventually.fulfilled.and.should.notify(done);    
+        });
+
+        it('that can be added without token', function(done) {
+            corbel.request.send({
+                url: currentResourcesEndpoint + domainId + '/resource/' + 
+                    publicResourceTestCollection,
+                method: corbel.request.method.POST,
+                headers: {
+                    'Accept': 'application/json'
+                }, 
+                data: {
+                    testField: 'testContent'
+                }
+            })
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                var locationSplitBar = response.headers.location.split('/');
+                resourceId = locationSplitBar[locationSplitBar.length - 1];
+                return corbel.request.send({
+                    url: currentResourcesEndpoint + domainId + '/resource/' + 
+                        publicResourceTestCollection + '/' +resourceId,
+                    method: corbel.request.method.GET,
+                    headers: {
+                        'Accept': 'application/json'
+                    }
+                })
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.id', resourceId);
+                expect(response).to.have.deep.property('data.testField', 'testContent');
             })
             .should.notify(done);
         });
