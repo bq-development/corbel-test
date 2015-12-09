2a0c8c248d7fbfd306cd08261ce2154b760b4e8f
Rubén Romero Córdoba <ruben.romero@bq.com>
2015-12-09 08:44:54 +0100

    migrated combine operations
    Change-Id: Idbcf5c9d530f14d6d04523904b834621fb73098b

---
+++ test/spec/resources/resmi/collection/aggregation/collectionCombineTest.js
@@ -0,0 +1,158 @@
+describe('In RESOURCES module', function () {
+    var corbelDriver;
+
+    before(function () {
+        corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+    });
+
+    describe('In RESMI module using combine operation', function () {
+
+        var COLLECTION = 'test:CombineCollection' + Date.now();
+        var amount = 10;
+        var extraField = {
+            field1: 10,
+            field2: 20
+        };
+
+        beforeEach(function (done) {
+            corbelTest.common.resources.createdObjectsToQuery(corbelDriver, COLLECTION, amount, extraField)
+                .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        afterEach(function (done) {
+            corbelTest.common.resources.cleanResourcesQuery(corbelDriver)
+                .should.be.eventually.fulfilled
+                .should.be.eventually.fulfilled.and.notify(done);
+        });
+
+        it('the collection elements with new calculated field are retrieved', function (done) {
+            var params = {
+                aggregation: {
+                    '$combine': {
+                        calculatedField: 'intField * computableField'
+                    }
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.length', amount);
+                    response.data.forEach(function (element) {
+                        var calculatedFieldValue = element.intField * element.computableField;
+                        expect(element).to.have.property('calculatedField', calculatedFieldValue);
+                    });
+                })
+                .should.notify(done);
+        });
+
+        it('the collection elements with new calculated field using map fields are retrieved', function (done) {
+            var params = {
+                aggregation: {
+                    '$combine': {
+                        calculatedField: 'extra.field1 * extra.field2'
+                    }
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.length', amount);
+                    response.data.forEach(function (element) {
+                        var calculatedFieldValue = element.extra.field1 * element.extra.field2;
+                        expect(element).to.have.property('calculatedField', calculatedFieldValue);
+                    });
+                })
+                .should.notify(done);
+        });
+
+        it('the collection elements with new calculated field sorted ascending using map fields are retrieved',
+            function (done) {
+            var params = {
+                sort: {
+                    calculatedField: 'asc'
+                },
+                aggregation: {
+                    '$combine': {
+                        calculatedField: 'intField * computableField'
+                    }
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.length', amount);
+                    expect(corbelTest.common.resources.checkSortingAsc(response.data,
+                        'calculatedField')).to.be.equal(true);
+                })
+                .should.notify(done);
+        });
+
+        it('the collection elements with new calculated field sorted descending using map fields are retrieved',
+            function (done) {
+            var params = {
+                sort: {
+                    calculatedField: 'desc'
+                },
+                aggregation: {
+                    '$combine': {
+                        calculatedField: 'intField * computableField'
+                    }
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.length', amount);
+                    expect(corbelTest.common.resources.checkSortingDesc(response.data, 'calculatedField'))
+                        .to.be.equal(true);
+                })
+                .should.notify(done);
+        });
+
+        it('the collection elements that intFields is within 300 & 500 with new calculated field sorted ascending ' +
+            'using map fields are retrieved', function (done) {
+            var params = {
+                query: [{
+                    '$gte': {
+                        intField: 300
+                    }
+                }, {
+                    '$lte': {
+                        intField: 500
+                    }
+                }],
+                sort: {
+                    calculatedField: 'desc'
+                },
+                aggregation: {
+                    '$combine': {
+                        calculatedField: 'intField * computableField'
+                    }
+                }
+            };
+
+            corbelDriver.resources.collection(COLLECTION)
+                .get(params)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.length', 3);
+                    expect(corbelTest.common.resources.checkSortingDesc(response.data,
+                        'calculatedField')).to.be.equal(true);
+                    response.data.forEach(function (element) {
+                        expect(element.intField).to.within(300, 500);
+                        var calculatedFieldValue = element.intField * element.computableField;
+                        expect(element).to.have.property('calculatedField', calculatedFieldValue);
+                    });
+                })
+                .should.notify(done);
+        });
+    });
+});
