5c26bd260ed5a556fb93b8d6cd0ef0e96963e54f
Cristian del Cerro <cristian.delcerro@bq.com>
2015-12-16 11:37:54 +0100

    consult plugins of evci test refactor to can do request to localhost
    Change-Id: I4c7f5ebff62cf47a1a41ece3e1f50d2787c8941e

--- test/spec/evci/pluginUpTest.js
+++ test/spec/evci/pluginUpTest.js
@@ -1,9 +1,11 @@
 describe('In EVCI module', function() {
 
     it('we can consult what eworkers are up', function(done) {
-        var url = corbelTest.CONFIG.COMMON.urlBase.replace('{{module}}', 'evci') ;
+    	var currentUrl = corbelTest.getConfig().localServices.indexOf('evci') !== -1 ? 
+            corbelTest.getConfig().evciEndpoint :
+            corbelTest.getConfig().urlBase.replace('{{module}}', 'evci');
 
-        corbelTest.common.utils.consultPlugins(url)
+        corbelTest.common.utils.consultPlugins(currentUrl)
         .should.be.eventually.fulfilled.and.should.notify(done);
     });           
 });
