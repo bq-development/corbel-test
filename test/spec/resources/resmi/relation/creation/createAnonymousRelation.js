6040b173406016532bc5cc8adc106efb396aae28
Ana Maria <anamaria.garcia@bq.com>
2015-12-18 13:16:27 +0100

    [SILKROAD-2242] [Patch 2] Reviewed create anonymous relation tests
    Change-Id: Iee29903f97890a3d793ae1ff974ef3bef48d7fc1

--- test/spec/resources/resmi/relation/creation/createAnonymousRelation.js
+++ test/spec/resources/resmi/relation/creation/createAnonymousRelation.js
@@ -1,95 +1,114 @@
 describe('In RESOURCES module', function() {
 
-    describe('In RESMI module ', function() {
+    describe('In RESMI module, while testing anonymous relation creation ', function() {
         var corbelDriver;
+
         var TIMESTAMP = Date.now();
-        var COLLECTION_A = 'test:CorbelJSRELATION_ANONYMOUS' + TIMESTAMP;
-        var RELATION_NAME = 'RELATION_NAME' + TIMESTAMP;
+        var COLLECTION_A = 'test:CreateAnonymousRelationError' + TIMESTAMP;
+        var RELATION_NAME = 'relationName' + TIMESTAMP;
         var TEST_OBJECT = {
             test: 'test'
         };
 
-        before(function() {
+        var resourceIdA;
+        var jsonRelationData = {
+            stringField: 'testCreateRelation'
+        };
+        var jsonRelationData2 = {
+            stringField: 'testCreateRelation2'
+        };
+
+        before(function(done) {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+
+            corbelDriver.resources.collection(COLLECTION_A)
+            .add(TEST_OBJECT)
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                resourceIdA = id;
+            })
+            .should.notify(done);
         });
 
-        describe('when testing AnonymusRelation creation', function() {
-            var idResourceA = 0;
-            var jsonRelationData = {
-                stringField: 'testCreateRelation'
-            };
-            var jsonRelationData2 = {
-                stringField: 'testCreateRelation2'
-            };
+        after(function(done) {
+            corbelDriver.resources.resource(COLLECTION_A, resourceIdA)
+            .delete()
+            .should.notify(done);
+        });
 
-            beforeEach(function(done) {
-                corbelDriver.resources.collection(COLLECTION_A).add(TEST_OBJECT)
-                .should.be.eventually.fulfilled
-                .then(function(id) {
-                    idResourceA = id;
-                })
-                .should.be.eventually.fulfilled.notify(done);
-            });
+        afterEach(function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+            .delete()
+            .should.eventually.be.fulfilled.and.notify(done);
+        });
 
-            afterEach(function(done) {
-                corbelDriver.resources.resource(COLLECTION_A, idResourceA)
-                .delete()
-                .should.be.eventually.fulfilled.notify(done);
-            });
+        it('an anonymous relation without extra data can be created', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+            .addAnonymous()
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+                .get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data[0].id', null);
+            })
+            .should.notify(done);
+        });
 
-            it('an anonymous relation without extra data can be created', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                .addAnonymous()
-                .should.be.eventually.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                    .get()
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function(response) {
-                    expect(response.data[0]._order).to.be.equal(1);
-                })
-                .should.notify(done);
-            });
+        it('an anonymous relation with extra data can be created', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+            .addAnonymous(jsonRelationData)
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+                .get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data[0].stringField', 'testCreateRelation');
+                expect(response).to.have.deep.property('data[0].id', null);
 
-            it('an anonymous relation with extra data can be created', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                .addAnonymous(jsonRelationData)
-                .should.be.eventually.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                    .get()
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function(response) {
-                    expect(response).to.have.deep.property('data[0].stringField', 'testCreateRelation');
-                    expect(response.data[0]._order).to.be.equal(1);
-                })
-                .should.notify(done);
-            });
+                return corbelDriver.resources.collection(COLLECTION_A)
+                .get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.not.have.deep.property('data.stringField');
+            })
+            .should.notify(done);
+        });
 
-            it('an anonymus relation with extra data can be created and updated', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                .addAnonymous(jsonRelationData)
-                .should.be.eventually.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                    .addAnonymous(jsonRelationData2)
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function() {
-                    return corbelDriver.resources.relation(COLLECTION_A, idResourceA, RELATION_NAME)
-                    .get()
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function(response) {
-                    expect(response).to.have.deep.property('data[0].stringField', 'testCreateRelation');
-                    expect(response).to.have.deep.property('data[0]._order', 1);
-                    expect(response).to.have.deep.property('data[1].stringField', 'testCreateRelation2');
-                    expect(response).to.have.deep.property('data[1]._order', 2);
-                })
-                .should.notify(done);
-            });
+        it('an anonymous relation with extra data can be created and more fields can be added', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+            .addAnonymous(jsonRelationData)
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+                .get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data[0].stringField', 'testCreateRelation');
+                expect(response).to.have.deep.property('data[0].id', null);
+
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+                .addAnonymous(jsonRelationData2)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, RELATION_NAME)
+                .get()
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                expect(response).to.have.deep.property('data[0].stringField', 'testCreateRelation');
+                expect(response).to.have.deep.property('data[0].id', null);
+                expect(response).to.have.deep.property('data[1].stringField', 'testCreateRelation2');
+                expect(response).to.have.deep.property('data[1].id', null);
+            })
+            .should.notify(done);
         });
     });
 });
--- test/spec/resources/resmi/relation/creation/createAnonymousRelationErrorTest.js
+++ test/spec/resources/resmi/relation/creation/createAnonymousRelationErrorTest.js
@@ -1,81 +1,79 @@
-describe('In RESOURCES module', function() {
+describe('In RESOURCES module ', function() {
 
-    describe('In RESMI module ', function() {
+    describe('In RESMI module, while testing anonymous relation creation ', function() {
         var corbelDriver;
+
         var TIMESTAMP = Date.now();
-        var COLLECTION_A = 'test:CorbelJSRELATION_ANONYMOUS' + TIMESTAMP;
-        var RELATION_NAME = 'RELATION_NAME' + TIMESTAMP;
+        var COLLECTION_A = 'test:CreateAnonymousRelationError' + Date.now();
+        var RELATION_NAME = 'relationName' + TIMESTAMP;
         var TEST_OBJECT = {
             test: 'test'
         };
 
-        before(function() {
+        var resourceIdA;
+        var jsonRelationData = {
+            stringField: 'testCreateRelation'
+        };
+        var jsonRelationData2 = {
+            stringField: 'testCreateRelation2'
+        };
+
+        before(function(done) {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+
+            corbelDriver.resources.collection(COLLECTION_A)
+            .add(TEST_OBJECT)
+            .should.be.eventually.fulfilled
+            .then(function(id) {
+                resourceIdA = id;
+            })
+            .should.notify(done);
         });
 
-        describe('when testing AnonymusRelation creation', function() {
-            var idResourceA = 0;
-            var jsonRelationData = {
-                stringField: 'testCreateRelation'
-            };
-            var jsonRelationData2 = {
-                stringField: 'testCreateRelation2'
-            };
+        after(function(done) {
+            corbelDriver.resources.resource(COLLECTION_A, resourceIdA)
+            .delete()
+            .should.notify(done);
+        });
 
-            beforeEach(function(done) {
-                corbelDriver.resources.collection(COLLECTION_A).add(TEST_OBJECT)
-                .should.be.eventually.fulfilled
-                .then(function(id) {
-                    idResourceA = id;
-                })
-                .should.be.eventually.fulfilled.notify(done);
-            });
+        it('400 is returned creating an anonymous relation with non-existent resource id & data', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, 'notExistingId', RELATION_NAME)
+            .addAnonymous(jsonRelationData)
+            .should.be.eventually.rejected.then(function(e) {
+                expect(e).to.have.property('status', 400);
+                expect(e.data).to.have.property('error', 'bad_request');
+            })
+            .should.notify(done);
+        });
 
-            afterEach(function(done) {
-                corbelDriver.resources.resource(COLLECTION_A, idResourceA)
-                .delete()
-                .should.be.eventually.fulfilled.notify(done);
-            });
+        it('400 is returned creating an anonymous relation with non-existent resource id & no data', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, 'notExistingId', RELATION_NAME)
+            .addAnonymous()
+            .should.be.eventually.rejected.then(function(e) {
+                expect(e).to.have.property('status', 400);
+                expect(e.data).to.have.property('error', 'bad_request');
+            })
+            .should.notify(done);
+        });
 
-            it('400 is returned creating an anonymous relation with unexistent resource id & data', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, 'notExistingId', RELATION_NAME)
-                .addAnonymous(jsonRelationData)
-                .should.be.eventually.rejected.then(function(e) {
-                    expect(e).to.have.property('status', 400);
-                    expect(e.data).to.have.property('error', 'bad_request');
-                })
-                .should.notify(done);
-            });
+        it('400 is returned creating an anonymous relation with non-existent collection', function(done) {
+            corbelDriver.resources.relation('test:non-existent', resourceIdA , RELATION_NAME)
+            .addAnonymous()
+            .should.be.eventually.rejected.then(function(e) {
+                expect(e).to.have.property('status', 400);
+                expect(e.data).to.have.property('error', 'bad_request');
+            })
+            .should.notify(done);
+        });
 
-            it('400 is returned creating an anonymous relation with unexistent resource id & no data', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, 'notExistingId', RELATION_NAME)
-                .addAnonymous()
-                .should.be.eventually.rejected.then(function(e) {
-                    expect(e).to.have.property('status', 400);
-                    expect(e.data).to.have.property('error', 'bad_request');
-                })
-                .should.notify(done);
-            });
-
-            it('400 is returned creating an anonymous relation with unexistent resource id & no data', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, 'notExistingId', RELATION_NAME)
-                .addAnonymous()
-                .should.be.eventually.rejected.then(function(e) {
-                    expect(e).to.have.property('status', 400);
-                    expect(e.data).to.have.property('error', 'bad_request');
-                })
-                .should.notify(done);
-            });
-
-            it('422 is returned creating an anonymous relation with malformed relation data', function(done) {
-                corbelDriver.resources.relation(COLLECTION_A, idResourceA , RELATION_NAME)
-                .addAnonymous('MALFORMED')
-                .should.be.eventually.rejected.then(function(e) {
-                    expect(e).to.have.property('status', 422);
-                    expect(e.data).to.have.property('error', 'invalid_entity');
-                })
-                .should.notify(done);
-            });
+        it('422 is returned creating an anonymous relation with malformed relation data', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA , RELATION_NAME)
+            .addAnonymous('MALFORMED')
+            .should.be.eventually.rejected.then(function(e) {
+                expect(e).to.have.property('status', 422);
+                expect(e.data).to.have.property('error', 'invalid_entity');
+            })
+            .should.notify(done);
         });
     });
 });
