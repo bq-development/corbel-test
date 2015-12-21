5b3b8e68309317384e42acbb1b12c8d580efd99a
Ana Maria <anamaria.garcia@bq.com>
2015-12-21 11:52:02 +0100

    [SILKROAD-2278] Reviewed get relation tests and added get relation test with _
    Change-Id: Ib22bef13e368a8e9f455c32280212b6b19fbf69c

--- test/spec/resources/resmi/relation/getting/getRelationAndErrorTest.js
+++ test/spec/resources/resmi/relation/getting/getRelationAndErrorTest.js
@@ -3,18 +3,18 @@ describe('In RESOURCES module', function() {
     describe('In RESMI module, testing get relation', function() {
         var corbelDriver;
         var TIMESTAMP = Date.now();
-        var COLLECTION_A = 'test:CoreJSObjectLinkA' + TIMESTAMP;
-        var COLLECTION_B = 'test:CoreJSObjectLinkB' + TIMESTAMP;
-        var idResourceA;
-        var idResourceB;
-        var idResourceC;
+        var COLLECTION_A = 'test:getRelationA' + TIMESTAMP;
+        var COLLECTION_B = 'test:getRelationB' + TIMESTAMP;
+        var resourceIdA;
+        var resourceIdB1;
+        var resourceIdB2;
 
         var TEST_OBJECT = {
             test: 'test'
         };
 
         var jsonRelationData = {
-            stringField: 'testCreateRelation'
+            stringField: 'testGetRelation'
         };
 
         var params = {
@@ -25,54 +25,55 @@ describe('In RESOURCES module', function() {
 
         before(function(done) {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+
             corbelDriver.resources.collection(COLLECTION_A)
             .add(TEST_OBJECT)
             .should.be.eventually.fulfilled
             .then(function(id) {
-                idResourceA = id;
+                resourceIdA = id;
 
                 return corbelDriver.resources.collection(COLLECTION_B)
                 .add(TEST_OBJECT)
                 .should.be.eventually.fulfilled;
             })
             .then(function(id) {
-                idResourceB = id;
+                resourceIdB1 = id;
 
                 return corbelDriver.resources.collection(COLLECTION_B)
                 .add(TEST_OBJECT)
                 .should.be.eventually.fulfilled;
             })
             .then(function(id) {
-                idResourceC = id;
+                resourceIdB2 = id;
 
-                return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
-                .add(idResourceB, jsonRelationData)
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, COLLECTION_B)
+                .add(resourceIdB1, jsonRelationData)
                 .should.be.eventually.fulfilled;
-                })
+            })
             .then(function() {
-                return corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
-                .add(idResourceC, jsonRelationData)
+                return corbelDriver.resources.relation(COLLECTION_A, resourceIdA, COLLECTION_B)
+                .add(resourceIdB2, jsonRelationData)
                 .should.be.eventually.fulfilled;
             })
             .should.notify(done);
         });
 
         after(function(done) {
-            corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, COLLECTION_B)
             .delete()
             .should.be.eventually.fulfilled
             .then(function() {
-                return corbelDriver.resources.resource(COLLECTION_A, idResourceA)
+                return corbelDriver.resources.resource(COLLECTION_A, resourceIdA)
                 .delete()
                 .should.be.eventually.fulfilled;
             })
             .then(function() {
-                return corbelDriver.resources.resource(COLLECTION_B, idResourceB)
+                return corbelDriver.resources.resource(COLLECTION_B, resourceIdB1)
                 .delete()
                 .should.be.eventually.fulfilled;
             })
             .then(function() {
-                return corbelDriver.resources.resource(COLLECTION_B, idResourceC)
+                return corbelDriver.resources.resource(COLLECTION_B, resourceIdB2)
                 .delete()
                 .should.be.eventually.fulfilled;
             })
@@ -80,23 +81,34 @@ describe('In RESOURCES module', function() {
         });
 
         it('all elements of a relation are returned', function(done) {
-            corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, COLLECTION_B)
             .get(null)
             .should.be.eventually.fulfilled
             .then(function(response) {
-                expect(response.data).to.have.length(2);
-                expect(response.data[0]).to.have.property('stringField', jsonRelationData.stringField);
-                expect(response.data[1]).to.have.property('stringField', jsonRelationData.stringField);
+                expect(response).to.have.deep.property('data.length', 2);
+                expect(response).to.have.deep.property('data[0].stringField', jsonRelationData.stringField);
+                expect(response).to.have.deep.property('.data[1].stringField', jsonRelationData.stringField);
             })
             .should.notify(done);
         });
 
         it('an specific element of the relation is returned', function(done) {
-            corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
-            .get(idResourceB)
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, COLLECTION_B)
+            .get(resourceIdB1)
             .should.be.eventually.fulfilled
             .then(function(response) {
-                expect(response.data.stringField).to.be.equal(jsonRelationData.stringField);
+                expect(response).to.have.deep.property('data.stringField', jsonRelationData.stringField);
+            })
+            .should.notify(done);
+        });
+
+        it('[UNDER_DEVELOPMENT] relations where destination id is resourceIdB1 are returned', function(done) {
+            corbelDriver.resources.relation(COLLECTION_A, '_', COLLECTION_B)
+            .get(resourceIdB1)
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.id', resourceIdA);
+                expect(response).to.have.deep.property('data.stringField', jsonRelationData.stringField);
             })
             .should.notify(done);
         });
@@ -106,14 +118,14 @@ describe('In RESOURCES module', function() {
             .get(null)
             .should.be.eventually.fulfilled
             .then(function(response) {
-                expect(response.data).to.have.property('length').to.be.equal(0);
+                expect(response).to.have.deep.property('data.length', 0);
             })
             .should.notify(done);
         });
 
         it('if the relation does not exist, 404 is returned trying to get an element', function(done) {
             corbelDriver.resources.relation(COLLECTION_A, 'notExistingId', COLLECTION_B)
-            .get(idResourceB)
+            .get(resourceIdB1)
             .should.be.eventually.rejected
             .then(function(response) {
                 expect(response).to.have.property('status', 404);
@@ -123,7 +135,7 @@ describe('In RESOURCES module', function() {
         });
 
         it('when trying to get a non existing resource from an existing relation, 404 is returned', function(done) {
-            corbelDriver.resources.relation(COLLECTION_A, idResourceA, COLLECTION_B)
+            corbelDriver.resources.relation(COLLECTION_A, resourceIdA, COLLECTION_B)
             .get('notExistingId')
             .should.be.eventually.rejected
             .then(function(e) {
