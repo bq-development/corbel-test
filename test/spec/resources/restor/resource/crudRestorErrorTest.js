01c7eb84068c79408ab2ebc2f95787ff9767d9f4
rmartinezcalvo <ricardo.martinez@bq.com>
2016-01-20 15:12:17 +0100

    [Patch 2]Restor tests improved
    Change-Id: Ifddade32c163da44df28a0051b4ce859641aadda

---
+++ test/spec/resources/restor/resource/crudRestorErrorTest.js
@@ -0,0 +1,32 @@
+describe('In RESOURCES module', function() {
+
+    describe('In RESTOR module', function() {
+        var corbelDriver;
+        var FOLDER_NAME = 'test:Restor';
+        var FILENAME = 'RestorFileName' + Date.now();
+
+        before(function() {
+            corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+        });
+
+        it('an error [404] is returned when you add a file using POST instead of PUT', function(done) {
+            corbelDriver.resources.resource(FOLDER_NAME, FILENAME).get({dataType:'application/octet-stream'})
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            }).
+            should.notify(done);
+        });
+
+        it('an error [404] is returned when you try to retrive a RESTOR file which does not exist', function(done) {
+            corbelDriver.resources.resource(FOLDER_NAME, FILENAME).get({dataType:'application/octet-stream'})
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+    });
+});
