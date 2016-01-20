cf9f7dee604af605f3ca676b430063cb7a748e05
rmartinezcalvo <ricardo.martinez@bq.com>
2016-01-20 15:12:17 +0100

    [Patch 3]Restor tests improved
    Change-Id: Ifddade32c163da44df28a0051b4ce859641aadda

--- test/spec/resources/restor/resource/crudRestorTest.js
+++ test/spec/resources/restor/resource/crudRestorTest.js
@@ -1,6 +1,6 @@
 describe('In RESOURCES module', function() {
 
-    describe('In RESTOR module, testing crudRestorResource', function() {
+    describe('In RESTOR module', function() {
         var corbelDriver;
         var FOLDER_NAME = 'test:Restor';
 
@@ -8,13 +8,44 @@ describe('In RESOURCES module', function() {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
         });
 
-        it.only('should make a complet flow for a file in RESTOR in octet-stream', function(done) {
+        it('should make a complet flow for a file in RESTOR in octet-stream sending a binary', function(done) {
             var FILENAME = 'RestorFileName' + Date.now();
-            var FILE_CONTENT = 'this Is My fileee!!! ññáaäéó' + Date.now();
+            var FILE_CONTENT = 'this Is My binary fileee!!! ññáaäéó' + Date.now();
             var BYTE_CONTENT = [];
             for(var i = 0; i < FILE_CONTENT.length; i++){
               BYTE_CONTENT.push(FILE_CONTENT.charCodeAt(i));
             }
+
+            corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+            .update(BYTE_CONTENT, {dataType: 'application/octet-stream'})
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/octet-stream'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(resource) {
+                expect(resource).to.have.property('data', FILE_CONTENT);
+
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .delete({dataType: 'application/octet-stream'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/octet-stream'})
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('should make a complet flow for a file in RESTOR in octet-stream sending an string', function(done) {
+            var FILENAME = 'RestorFileName' + Date.now();
+            var FILE_CONTENT = 'this Is My string fileee!!! ññáaäéó' + Date.now();
 
             corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
             .update(FILE_CONTENT, {dataType: 'application/octet-stream'})
@@ -25,7 +56,7 @@ describe('In RESOURCES module', function() {
                 .should.be.eventually.fulfilled;
             })
             .then(function(resource) {
-                expect(resource).to.have.property('data', BYTE_CONTENT);
+                expect(resource).to.have.property('data', FILE_CONTENT);
 
                 return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
                 .delete({dataType: 'application/octet-stream'})
