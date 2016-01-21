82b0e1b978a338447e6cf63b063ad880312676ce
rmartinezcalvo <ricardo.martinez@bq.com>
2016-01-21 10:17:57 +0100

    Restor tests improved
    Change-Id: I09269e8aecf06fe8a8299712f87110f1810c2401

--- test/spec/resources/restor/resource/crudRestorTest.js
+++ test/spec/resources/restor/resource/crudRestorTest.js
@@ -1,5 +1,6 @@
 describe('In RESOURCES module', function() {
-    describe('In RESTOR module, testing crudRestorResource', function() {
+
+    describe('In RESTOR module, while testing CRUD operations', function() {
         var corbelDriver;
         var FOLDER_NAME = 'test:Restor';
 
@@ -7,9 +8,45 @@ describe('In RESOURCES module', function() {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
         });
 
-        var FILENAME = 'RestorFileName' + Date.now();
-        var FILE_CONTENT = 'this Is My fileee!!! ññáaäéó' + Date.now();
-        it.skip('should make a complet flow for a file in RESTOR in octet-stream', function(done) {
+        it('should make a complet flow for a file in RESTOR in octet-stream sending a binary', function(done) {
+            var FILENAME = 'RestorFileName' + Date.now();
+            var FILE_CONTENT = 'this Is My binary fileee!!! ññáaäéó' + Date.now();
+            var BYTE_CONTENT = [];
+            for(var i = 0; i < FILE_CONTENT.length; i++){
+              BYTE_CONTENT.push(FILE_CONTENT.charCodeAt(i));
+            }
+
+            corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+            .update(BYTE_CONTENT, {dataType: 'application/octet-stream'})
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/octet-stream'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(resource) {
+                expect(resource).to.have.property('data', FILE_CONTENT);
+
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .delete({dataType: 'application/octet-stream'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/octet-stream'})
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('should make a complet flow for a file in RESTOR in octet-stream sending a string', function(done) {
+            var FILENAME = 'RestorFileName' + Date.now();
+            var FILE_CONTENT = 'this Is My string fileee!!! ññáaäéó' + Date.now();
+
             corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
             .update(FILE_CONTENT, {dataType: 'application/octet-stream'})
             .should.be.eventually.fulfilled
@@ -34,12 +71,7 @@ describe('In RESOURCES module', function() {
                 expect(e).to.have.property('status', 404);
                 expect(e).to.have.deep.property('data.error', 'not_found');
             })
-            .then(function() {
-                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
-                .delete({dataType: 'application/octet-stream'})
-                .should.be.eventually.fulfilled;
-            })
-            .should.be.eventually.fulfilled.and.notify(done);
+            .should.notify(done);
         });
 
         it('should make a complet flow for a file in RESTOR in XML', function(done) {
@@ -70,12 +102,7 @@ describe('In RESOURCES module', function() {
                 expect(e).to.have.property('status', 404);
                 expect(e).to.have.deep.property('data.error', 'not_found');
             })
-            .then(function() {
-                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
-                .delete({dataType: 'application/xml'})
-                .should.be.eventually.fulfilled;
-            })
-            .should.be.eventually.fulfilled.and.notify(done);
+            .should.notify(done);
         });
     });
 });
