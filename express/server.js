415bf641c0bfe7f7ee8dd697bea88bbe1780011e
rmartinezcalvo <ricardo.martinez@bq.com>
2016-01-21 15:43:13 +0100

    [Patch 7]epubGenerator for express server
    Change-Id: I987676f9b86d4a6f3b345009537f5a9904826aad

--- express/server.js
+++ express/server.js
@@ -1,14 +1,16 @@
 var express = require('express');
+<<<<<<< fa38be0aa03d3f63d089d615d6045e70670e08f9
 var request = require('request');
 var ports = require('../test/ports.conf.js');
+=======
+var bodyParser = require('body-parser');
+>>>>>>> [Patch 7]epubGenerator for express server
 
-var API_EMAIL_ENDPOINT = 'http://api.guerrillamail.com/ajax.php';
-var API_RANDOM_EMAIL_SUFFIX = 'f=get_email_address';
-var API_SET_EMAIL_SUFFIX = 'f=set_email_user';
-var API_CHECK_EMAIL_SUFFIX = 'f=check_email&seq=1';
-var API_GET_EMAIL_SUFFIX = 'f=fetch_email';
 // Set up the express server
 var app = express();
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:'+ (ports.KARMA || 3000));
@@ -27,7 +29,11 @@ console.log('server started');
 [
   './emails/random.js',
   './emails/imap.js',
+<<<<<<< fa38be0aa03d3f63d089d615d6045e70670e08f9
   './requestinfo/requestinfo.js'
+=======
+  './binaries/epubGenerator.js'
+>>>>>>> [Patch 7]epubGenerator for express server
 ].forEach(function(routePath){
     require(routePath)(app);
 });
