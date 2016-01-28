125d592c8c4a36c8ef00d4a4106723677dff6a18
rmartinezcalvo <ricardo.martinez@bq.com>
2016-01-28 17:48:00 +0100

    [Patch 4]restor tests improved
    Change-Id: I2f9b6beaea19bbb2d854c2fc02898084711147e6

--- test/spec/resources/restor/resource/crudRestorTest.js
+++ test/spec/resources/restor/resource/crudRestorTest.js
@@ -8,7 +8,7 @@ describe('In RESOURCES module', function() {
             corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
         });
 
-        it('should make a complet flow for a file in RESTOR in octet-stream sending a binary', function(done) {
+        it('should make a complet flow for a file in RESTOR in octet-stream sending a binary array', function(done) {
             var FILENAME = 'RestorFileName' + Date.now();
             var FILE_CONTENT = 'this Is My binary fileee!!! ññáaäéó' + Date.now();
             var BYTE_CONTENT = [];
@@ -25,7 +25,7 @@ describe('In RESOURCES module', function() {
                 .should.be.eventually.fulfilled;
             })
             .then(function(resource) {
-                expect(resource).to.have.property('data', FILE_CONTENT);
+                expect(resource).to.have.property('data', BYTE_CONTENT.toString());
 
                 return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
                 .delete({dataType: 'application/octet-stream'})
@@ -65,6 +65,72 @@ describe('In RESOURCES module', function() {
             .then(function() {
                 return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
                 .get({dataType: 'application/octet-stream'})
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('should make a complet flow for a file in RESTOR in blob sending a binary array', function(done) {
+            var FILENAME = 'RestorFileName' + Date.now();
+            var FILE_CONTENT = 'this Is My binary fileee!!! ññáaäéó' + Date.now();
+            var BYTE_CONTENT = [];
+            for(var i = 0; i < FILE_CONTENT.length; i++){
+              BYTE_CONTENT.push(FILE_CONTENT.charCodeAt(i));
+            }
+
+            corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+            .update(BYTE_CONTENT, {dataType: 'application/blob'})
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/blob'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(resource) {
+                expect(resource).to.have.property('data', BYTE_CONTENT.toString());
+
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .delete({dataType: 'application/blob'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/blob'})
+                .should.be.eventually.rejected;
+            })
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
+        });
+
+        it('should make a complet flow for a file in RESTOR in blob sending a string', function(done) {
+            var FILENAME = 'RestorFileName' + Date.now();
+            var FILE_CONTENT = 'this Is My string fileee!!! ññáaäéó' + Date.now();
+
+            corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+            .update(FILE_CONTENT, {dataType: 'application/blob'})
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/blob'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(resource) {
+                expect(resource).to.have.property('data', FILE_CONTENT);
+
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .delete({dataType: 'application/blob'})
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelDriver.resources.resource(FOLDER_NAME, FILENAME)
+                .get({dataType: 'application/blob'})
                 .should.be.eventually.rejected;
             })
             .then(function(e) {
