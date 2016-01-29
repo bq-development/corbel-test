4c55a158aa4d01c0e02baca90daf59eb483a1dc2
rmartinezcalvo <ricardo.martinez@bq.com>
2016-01-29 11:30:48 +0100

    [Patch 3]private routes availability for express
    Change-Id: Idd922f79795ffe0c383be37dd6a0f5da50900291

--- express/server.js
+++ express/server.js
@@ -1,15 +1,17 @@
 var express = require('express');
+<<<<<<< fa38be0aa03d3f63d089d615d6045e70670e08f9
 var request = require('request');
 var ports = require('../test/ports.conf.js');
+=======
+var fs = require('fs');
+var _ = require('lodash');
+var glob = require('glob');
+>>>>>>> [Patch 3]private routes availability for express
 
-var API_EMAIL_ENDPOINT = 'http://api.guerrillamail.com/ajax.php';
-var API_RANDOM_EMAIL_SUFFIX = 'f=get_email_address';
-var API_SET_EMAIL_SUFFIX = 'f=set_email_user';
-var API_CHECK_EMAIL_SUFFIX = 'f=check_email&seq=1';
-var API_GET_EMAIL_SUFFIX = 'f=fetch_email';
 // Set up the express server
 var app = express();
 
+//Middlewares
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:'+ (ports.KARMA || 3000));
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -17,13 +19,33 @@ app.use(function(req, res, next) {
   next();
 });
 
+//Routes initializations
 app.get('/', function(req, res) {
     res.send('Corbel-js express server');
 });
 
+//Corbel opensource services test
+var publicRoutes = [
+  './emails/random.js',
+  './emails/imap.js'
+];
+
+//Only available for non-opensource resources
+var privateRoutes = glob.sync('../test/spec/private/express/**/*.js');
+
+function addToServer(path){
+  require(path)(app);
+}
+
+_.union(publicRoutes, privateRoutes)
+  .filter(fs.existsSync)
+  .forEach(addToServer);
+
+
 app.listen(process.env.PORT || 3000);
 console.log('server started');
 
+<<<<<<< fa38be0aa03d3f63d089d615d6045e70670e08f9
 [
   './emails/random.js',
   './emails/imap.js',
@@ -31,5 +53,7 @@ console.log('server started');
 ].forEach(function(routePath){
     require(routePath)(app);
 });
+=======
+>>>>>>> [Patch 3]private routes availability for express
 
 module.exports = app;
