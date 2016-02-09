46fd043b510515a217f6759899ef75c4ecb3d36a
Rubén Romero Córdoba <ruben.romero@bq.com>
2016-02-09 17:06:27 +0100

    get express port from config file
    Change-Id: I9d809d7f970c5ca9ec50122a1a214c114616142e

--- src/common/mail/randomMail.js
+++ src/common/mail/randomMail.js
@@ -3,18 +3,21 @@
 /*globals corbel */
 //@endexclude
 
-/* The response of this method is an object with a random email data and cookies
-* with the session token
-*/
-function getRandomMail(){
-    var xhttp = new XMLHttpRequest();
-    var url = 'http://localhost:5454/random/randomemail';
+var PORTS = require('../../../test/ports.conf.js');
 
-    return new Promise(function(resolve, reject){
-        xhttp.onreadystatechange = function() {
+
+/* The response of this method is an object with a random email data and cookies
+ * with the session token
+ */
+function getRandomMail() {
+    var xhttp = new XMLHttpRequest();
+    var url = 'http://localhost:' + PORTS.EXPRESS + '/random/randomemail';
+
+    return new Promise(function (resolve, reject) {
+        xhttp.onreadystatechange = function () {
             if (xhttp.readyState === 4 && xhttp.status === 200) {
                 resolve(JSON.parse(xhttp.responseText));
-            } 
+            }
         };
 
         xhttp.open('GET', url, true);
@@ -23,15 +26,15 @@ function getRandomMail(){
 }
 
 /* userEmail must be the data before @ */
-function setMail(token, userEmail){
+function setMail(token, userEmail) {
     var xhttp = new XMLHttpRequest();
-    var url = 'http://localhost:5454/random/setemail?token=' + token + '&userEmail=' + userEmail;
+    var url = 'http://localhost:' + PORTS.EXPRESS + '/random/setemail?token=' + token + '&userEmail=' + userEmail;
 
-    return new Promise(function(resolve, reject){
-        xhttp.onreadystatechange = function() {
+    return new Promise(function (resolve, reject) {
+        xhttp.onreadystatechange = function () {
             if (xhttp.readyState === 4 && xhttp.status === 200) {
                 resolve(JSON.parse(xhttp.responseText));
-            } 
+            }
         };
 
         xhttp.open('GET', url, true);
@@ -40,26 +43,26 @@ function setMail(token, userEmail){
 }
 
 /* The response of this method is a list of messages, represented as an array of objects and cookies
-* with the session token
-*
-* Each object has the following properties:
-* ‘mail_id’, 
-* ‘mail_from’ (email address of sender),
-* ‘mail_subject’,
-* ‘mail_excerpt’ (snippet from the email),
-* ‘mail_timestamp’ (a UNIX timestamp),
-* ‘mail_read’ (1 if read, 0 if not),
-* ‘mail_date’
-*/
-function checkMail(token){
+ * with the session token
+ *
+ * Each object has the following properties:
+ * ‘mail_id’,
+ * ‘mail_from’ (email address of sender),
+ * ‘mail_subject’,
+ * ‘mail_excerpt’ (snippet from the email),
+ * ‘mail_timestamp’ (a UNIX timestamp),
+ * ‘mail_read’ (1 if read, 0 if not),
+ * ‘mail_date’
+ */
+function checkMail(token) {
     var xhttp = new XMLHttpRequest();
-    var url = 'http://localhost:5454/random/checkemail?token=' + token;
+    var url = 'http://localhost:' + PORTS.EXPRESS + '/random/checkemail?token=' + token;
 
-    return new Promise(function(resolve, reject){
-        xhttp.onreadystatechange = function() {
+    return new Promise(function (resolve, reject) {
+        xhttp.onreadystatechange = function () {
             if (xhttp.readyState === 4 && xhttp.status === 200) {
                 resolve(JSON.parse(xhttp.responseText));
-            } 
+            }
         };
 
         xhttp.open('GET', url, true);
@@ -68,23 +71,55 @@ function checkMail(token){
 }
 
 /* The response of this method is an object with the following properties:
-* ‘mail_id’, 
-* ‘mail_from’ (email address of sender),
-* ‘mail_subject’,
-* ‘mail_excerpt’ (snippet from the email),
-* ‘mail_timestamp’ (a UNIX timestamp),
-* ‘mail_read’ (1 if read, 0 if not),
-* ‘mail_date’
-*/
-function getMail(token, emailId){
+ * ‘mail_id’,
+ * ‘mail_from’ (email address of sender),
+ * ‘mail_subject’,
+ * ‘mail_excerpt’ (snippet from the email),
+ * ‘mail_timestamp’ (a UNIX timestamp),
+ * ‘mail_read’ (1 if read, 0 if not),
+ * ‘mail_date’
+ */
+function getMail(token, emailId) {
     var xhttp = new XMLHttpRequest();
-    var url = 'http://localhost:5454/random/getemail?token=' + token + '&emailId=' + emailId;
+    var url = 'http://localhost:' + PORTS.EXPRESS + '/random/getemail?token=' + token + '&emailId=' + emailId;
 
-    return new Promise(function(resolve, reject){
-        xhttp.onreadystatechange = function() {
+    return new Promise(function (resolve, reject) {
+        xhttp.onreadystatechange = function () {
             if (xhttp.readyState === 4 && xhttp.status === 200) {
                 resolve(JSON.parse(xhttp.responseText));
-            } 
+            }
+        };
+
+        xhttp.open('GET', url, true);
+        xhttp.send();
+    });
+}
+
+/* Get the contents of an email.
+ * NOTE: All HTML in the body of the email is filtered. Eg, Javascript, applets, iframes, etc is removed.
+ *
+ *
+ * mail_id - an integer
+ * mail_from - email address of sender
+ * mail_recipient - email address of receiver
+ * mail_subject - UTF-8 encoded string of the email’s subject
+ * mail_excerpt - a short excerpt of the email
+ * mail_body - The message part of the email. May contain filtered HTML as described above
+ * mail_timestamp - unix timestamp of arrival
+ * mail_date - Date of arrival in the following format YYYY:MM:DD H:M:S
+ * mail_read - 0 or 1 indicating if email was fetched before
+ * content_type - MIME type for the mail_body, can be text/plain or text/html
+ * ‘sid_token’ - Session ID token
+ */
+function getMailContent(emailId) {
+    var xhttp = new XMLHttpRequest();
+    var url = 'http://localhost:' + PORTS.EXPRESS + '/random/getemailcontent?emailId=' + emailId;
+
+    return new Promise(function (resolve, reject) {
+        xhttp.onreadystatechange = function () {
+            if (xhttp.readyState === 4 && xhttp.status === 200) {
+                resolve(JSON.parse(xhttp.responseText));
+            }
         };
 
         xhttp.open('GET', url, true);
@@ -101,7 +136,6 @@ function getCodeFromMail(email) {
         throw new Error('Mail withouth code');
     }
 }
-
 
 module.exports = {
     getRandomMail: getRandomMail,
