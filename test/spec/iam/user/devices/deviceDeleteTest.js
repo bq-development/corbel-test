9adb3ee781dbc3d4be32e3fe8f04686539347fe3
Francisco Sanchez <francisco.sanchez@bq.com>
2016-02-18 13:04:30 +0100

    [SILKROAD-2391] Device delete should be invalidate tokens
    Change-Id: I597d60391e01ce3e99ef0e7f8e123e261522b030

---
+++ test/spec/iam/user/devices/deviceDeleteTest.js
@@ -0,0 +1,76 @@
+describe('In IAM module', function() {
+    describe('while testing devices', function() {
+        var loginUser = corbelTest.common.clients.loginUser;
+        var getTokenInfo = corbelTest.common.utils.getTokenInfo;
+        var retry = corbelTest.common.utils.retry;
+
+        var MAX_RETRY = 10;
+        var RETRY_PERIOD = 1;
+
+        var corbelDriver;
+        var deviceId;
+
+        beforeEach(function(done) {
+            var user;
+            corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+
+            corbelTest.common.iam.createUsers(corbelDriver, 1)
+                .should.be.eventually.fulfilled
+                .then(function(users) {
+                    user = users[0];
+                    return loginUser(corbelDriver, user.username, user.password)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function() {
+                    var device = {
+                        notificationUri: Date.now(),
+                        uid: Date.now(),
+                        name: 'device',
+                        type: 'Android',
+                        notificationEnabled: true
+                    };
+                    return corbelDriver.iam.user()
+                        .registerMyDevice(device)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function(deviceIdResponse) {
+                    deviceId = deviceIdResponse;
+                    return loginUser(corbelDriver, user.username, user.password, deviceId)
+                        .should.be.eventually.fulfilled;
+                })
+                .should.notify(done);
+        });
+
+        afterEach(function(done) {
+            corbelDriver.iam.user('me')
+                .delete()
+                .should.be.eventually.fulfilled
+                .and.notify(done);
+        });
+
+        it('when a user deletes the device that he used in the login, the token is invalidated', function(done) {
+            var retriveDevice;
+
+            var tokenInfo = getTokenInfo(corbelDriver);
+            expect(tokenInfo).to.have.deep.property('info.deviceId', deviceId);
+
+            corbelDriver.iam.user()
+                .deleteMyDevice(deviceId)
+                .should.be.eventually.fulfilled
+                .then(function() {
+                    return retry(function() {
+                        return corbelDriver.iam.user()
+                            .get()
+                            .should.be.eventually.fulfilled
+                            .then(function() {
+                                var tokenInfo = getTokenInfo(corbelDriver);
+                                console.log(tokenInfo.token);
+                                expect(tokenInfo).to.not.have.deep.property('info.deviceId');
+                            });
+                    }, MAX_RETRY, RETRY_PERIOD);
+                })
+
+            .should.notify(done);
+        });
+    });
+});
