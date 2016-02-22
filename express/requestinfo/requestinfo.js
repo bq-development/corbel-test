fd0ea3495cc624e392485fe5cbda0d369bc98942
Rubén Romero Córdoba <ruben.romero@bq.com>
2016-02-22 08:34:22 +0100

    host param is deprecated, its changed by hostname instead
    Change-Id: I8179b156e2980d3b95be4eba39a82c774f54ba96

--- express/requestinfo/requestinfo.js
+++ express/requestinfo/requestinfo.js
@@ -3,18 +3,18 @@
 module.exports = function setup(app) {
 
     app.get('/requestinfo', function (req, res) {
-        var PORT = process.env.PORT || 3000;
+        var PORT = process.env.PORT || 3000;
 
         res.json({
             query: req.query,
             ip: req.ip,
             path: req.path,
             port: PORT,
-            host: req.host,
+            host: req.hostname,
             protocol: req.protocol,
             subdomains: req.subdomains,
             headers: req.headers,
-            url: req.protocol + '://' + req.host + ( PORT === 80 || PORT === 443 ? '' : ':' + PORT ) + req.path
+            url: req.protocol + '://' + req.hostname + ( PORT === 80 || PORT === 443 ? '' : ':' + PORT ) + req.path
         });
     });
 };
