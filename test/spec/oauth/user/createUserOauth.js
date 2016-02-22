2e83393d28463166dd81fc98cc65006602eea430
Rubén Romero Córdoba <ruben.romero@bq.com>
2016-02-22 15:57:06 +0100

    oauth user tests added
    Change-Id: Ibd16763c18a4713ca418c313a2a3d0e8891dadd3

---
+++ test/spec/oauth/user/createUserOauth.js
@@ -0,0 +1,62 @@
+describe('In OAUTH module', function () {
+
+    var corbelDriver;
+    var oauthCommon;
+    var oauthUserTest;
+
+    before(function () {
+        corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
+        oauthCommon = corbelTest.common.oauth;
+        oauthUserTest = oauthCommon.getOauthUserTestParams();
+    });
+
+
+    describe('when requests to create a new user', function () {
+
+        it('successes with response CREATED (201)', function (done) {
+            var token;
+            var random = Date.now();
+
+            var userTest = {
+                'username': 'createUserOauthTest' + random,
+                'password': 'passwordTest',
+                'email': 'createUserOauthTest' + random + '@funkifake.com'
+            };
+
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams())
+                .create(userTest)
+                .should.be.eventually.fulfilled
+                .then(function () {
+                    return corbelDriver.oauth
+                        .authorization(oauthCommon.getClientParamsCode())
+                        .login(userTest.username, userTest.password)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    return corbelDriver.oauth
+                        .token(oauthCommon.getClientParamsToken())
+                        .get(response.data.query.code)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    token = response.data['access_token'];
+
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), token)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                }).then(function (response) {
+                    expect(response).to.have.deep.property('data.email', userTest.email.toLowerCase());
+                    expect(response).to.have.deep.property('data.username', userTest.username.toLowerCase());
+                    expect(response).to.have.deep.property('data.role', 'USER');
+
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), token)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .should.notify(done);
+        });
+    });
+});
---
+++ test/spec/oauth/user/updateUserOauthTest.js
@@ -0,0 +1,263 @@
+describe('In OAUTH module', function () {
+
+    describe('when request to update user information', function () {
+
+        var corbelDriver;
+        var oauthCommon;
+        var oauthAdminUserTest, oauthRootUserTest;
+        var adminAccessToken;
+        var accessToken;
+        var userTest;
+        var timeStamp;
+
+        before(function (done) {
+            corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
+            oauthCommon = corbelTest.common.oauth;
+            oauthAdminUserTest = oauthCommon.getOauthAdminUserParams();
+            oauthRootUserTest = oauthCommon.getOauthRootUserParams();
+
+            oauthCommon.getToken(corbelDriver, oauthAdminUserTest.username, oauthAdminUserTest.password)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    adminAccessToken = response.data['access_token'];
+                })
+                .should.notify(done);
+        });
+
+
+        beforeEach(function (done) {
+            timeStamp = Date.now();
+            userTest = {
+                'username': 'updateUserOauthTest' + timeStamp,
+                'password': 'passwordTest',
+                'email': 'updateUserOauthTes' + timeStamp + '@funkifake.com',
+                'properties': {
+                    'propertyKey1': 'propertyValue1',
+                    'propertyKey2': 'propertyValue2'
+                }
+            };
+
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams())
+                .create(userTest)
+                .should.be.eventually.fulfilled
+                .then(function () {
+                    return oauthCommon
+                        .getToken(corbelDriver, userTest.username, userTest.password)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    accessToken = response.data['access_token'];
+                })
+                .should.notify(done);
+        });
+
+        afterEach(function (done) {
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .delete('me')
+                .should.be.eventually.fulfilled.should.notify(done);
+        });
+
+        it('204 is returned when request update user email', function (done) {
+            var updatedEmail = 'updateUserOauthTestUpdated' + timeStamp + '@funkifake.com';
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .update('me', {'email': updatedEmail})
+                .should.be.eventually.fulfilled
+                .then(function () {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), accessToken)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.email', updatedEmail.toLowerCase());
+                })
+                .should.notify(done);
+        });
+
+        it('204 is returned when request update user password', function (done) {
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .update('me', {'password': 'passwordTestUpdate'})
+                .should.be.eventually.fulfilled
+                .then(function () {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), accessToken)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.email', userTest.email.toLowerCase());
+                })
+                .should.notify(done);
+        });
+
+        it('304 is returned when request update user role', function (done) {
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .update('me', {'role': 'ADMIN'})
+                .should.be.eventually.rejected
+                .then(function (response) {
+                    expect(response).to.have.property('status', 403);
+                })
+                .should.notify(done);
+        });
+
+        it('204 is returned when request update other user email', function (done) {
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .get('me')
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), adminAccessToken)
+                        .update(response.data.id, {
+                            'email': 'createUserOauthTest' + timeStamp + '@funkifake.com'
+                        });
+                })
+                .should.be.eventually.fulfilled.should.notify(done);
+        });
+
+        it('204 is returned when request update other user email and role', function (done) {
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .get('me')
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), adminAccessToken)
+                        .update(response.data.id, {
+                            'email': 'createUserOauthTest' + timeStamp + '@funkifake.com',
+                            'role': 'USER'
+                        });
+                })
+                .should.be.eventually.fulfilled.should.notify(done);
+        });
+
+        it('403 is returned when request to update user role to ROOT', function (done) {
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .get('me')
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), adminAccessToken)
+                        .update(response.data.id, {
+                            'email': 'createUserOauthTest' + timeStamp + '@funkifake.com',
+                            'role': 'ROOT'
+                        });
+                })
+                .should.be.eventually.rejected
+                .then(function (response) {
+                    expect(response).to.have.property('status', 403);
+                })
+                .should.notify(done);
+        });
+
+        it('204 is returned when request update properties changing values', function (done) {
+            var propertiesUpdate = {
+                'propertyKey1': 'propertyValueUpdate1',
+                'propertyKey2': 'propertyValueUpdate2'
+            };
+
+            corbelDriver.oauth.user(oauthCommon.getClientParams(), accessToken)
+                .update('me', {'properties': propertiesUpdate})
+                .should.be.eventually.fulfilled
+                .then(function () {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), accessToken)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.properties.propertyKey1', 'propertyValueUpdate1');
+                    expect(response).to.have.deep.property('data.properties.propertyKey2', 'propertyValueUpdate2');
+                })
+                .should.notify(done);
+        });
+
+        it('204 is returned when request update properties adding more properties', function (done) {
+            var propertiesUpdate = {
+                'propertyKey3': 'propertyValueUpdate3'
+            };
+
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .update('me', {'properties': propertiesUpdate})
+                .should.be.eventually.fulfilled.then(function () {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), accessToken)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    expect(response).to.have.deep.property('data.properties.propertyKey1', 'propertyValue1');
+                    expect(response).to.have.deep.property('data.properties.propertyKey2', 'propertyValue2');
+                    expect(response).to.have.deep.property('data.properties.propertyKey3', 'propertyValueUpdate3');
+                })
+                .should.notify(done);
+        });
+
+        it('204 is returned when request update properties removing a property ', function (done) {
+            var propertiesUpdate = {
+                'propertyKey1': null
+            };
+
+            corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), accessToken)
+                .update('me', {'properties': propertiesUpdate})
+                .should.be.eventually.fulfilled
+                .then(function () {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), accessToken)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    expect(response).to.not.have.deep.property('data.properties.propertyKey1');
+                    expect(response).to.have.deep.property('data.properties.propertyKey2', 'propertyValue2');
+                })
+                .should.notify(done);
+        });
+
+        it('404 is returned when request to update root user with admin user', function (done) {
+            var rootAccessToken;
+
+            oauthCommon
+                .getToken(corbelDriver, oauthRootUserTest.username, oauthRootUserTest.password)
+                .should.be.eventually.fulfilled
+                .then(function (response) {
+                    rootAccessToken = response.data['access_token'];
+
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), rootAccessToken)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), adminAccessToken)
+                        .update(response.data.id, {'email': 'createUserOauthTest' + timeStamp + '@funkifake.com'});
+                })
+                .should.be.eventually.rejected
+                .then(function (response) {
+                    expect(response).to.have.property('status', 404);
+                })
+                .should.notify(done);
+        });
+
+        it('401 is returned when request to get user details and user not exist', function (done) {
+
+            return corbelDriver.oauth
+                .user(oauthCommon.getClientParams(), 'BAD ACCESS TOKEN')
+                .update('me')
+                .should.be.eventually.rejected
+                .then(function (response) {
+                    expect(response).to.have.property('status', 401);
+                })
+                .should.notify(done);
+        });
+    });
+});
