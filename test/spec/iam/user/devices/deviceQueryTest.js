425caf5f1d156f917b760013bdb5f4dcf6842603
Francisco Sanchez <francisco.sanchez@bq.com>
2016-02-25 18:02:40 +0100

    [SILKROAD-2439] Add test to query with getDevices
    Change-Id: I75b69fd80fb9926d5eb543cd9d94ec0d76f8718f

---
+++ test/spec/iam/user/devices/deviceQueryTest.js
@@ -0,0 +1,65 @@
+describe('In IAM module', function() {
+
+    describe('while testing devices', function() {
+        var TOTAL_DEVICE_AMOUNT = 10;
+        var user;
+        var corbelDriver;
+        var random;
+        var retriveDevices;
+
+        beforeEach(function(done) {
+            corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+            corbelTest.common.iam.createUsers(corbelDriver, 1)
+                .should.be.eventually.fulfilled
+                .then(function(createdUsers) {
+                    user = createdUsers[0];
+                    return corbelTest.common.clients.loginUser(corbelDriver, user.username, user.password)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function() {
+                    return corbelTest.common.iam.createDevices(corbelDriver, TOTAL_DEVICE_AMOUNT)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function() {
+                    return corbelDriver.iam.user()
+                        .getMyDevices()
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function(devices) {
+                    expect(devices).to.have.deep.property('data.length', TOTAL_DEVICE_AMOUNT);
+                    expect(devices.data).to.all.have.property('userId', user.id);
+                    retriveDevices = devices.data;
+                })
+                .should.notify(done);
+        });
+
+        afterEach(function(done) {
+            corbelDriver.iam.user()
+                .deleteMe()
+                .should.be.eventually.fulfilled
+                .and.should.notify(done);
+        });
+
+        it('can get a page ordered', function(done) {
+            var params = {
+                sort: {
+                    _createdAt: 'asc'
+                },
+                pagination: {
+                    page: 0,
+                    pageSize: 3
+                }
+            };
+
+            corbelDriver.iam.user()
+                .getMyDevices(params)
+                .should.be.eventually.fulfilled
+                .then(function(devices) {
+                    expect(devices).to.have.deep.property('data.length', 3);
+                    expect(devices.data).to.all.have.property('userId', user.id);
+                })
+                .should.notify(done);
+        });
+
+    });
+});
