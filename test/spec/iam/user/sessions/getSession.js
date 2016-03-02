22a0b7d7d4442955c9a466c88698b31c7ba5ea02
vipereybq <victor.perezrey@bq.com>
2016-03-02 13:31:21 +0100

    [Patch 2][SILKROAD-2454 > SILKROAD-2456] GET user/me/session test
    Change-Id: I8b86bcb5e6d412953d517203e2bb29f6dfaa5043

---
+++ test/spec/iam/user/sessions/getSession.js
@@ -0,0 +1,41 @@
+describe('In IAM module', function() {
+    var corbelDriver;
+    var user;
+
+    describe('while testing get a user sessions', function() {
+
+        before(function(done) {
+            corbelDriver = corbelTest.drivers['DEFAULT_CLIENT'].clone();
+
+            corbelTest.common.iam.createUsers(corbelDriver, 1)
+            .should.be.eventually.fulfilled
+            .then(function(createdUsers) {
+                user = createdUsers[0];
+
+                return corbelTest.common.clients.loginUser(corbelDriver, user.username, user.password)
+                .should.eventually.be.fulfilled;
+            })
+            .should.notify(done);
+        });
+
+        after(function(done) {
+            corbelDriver.iam.user()
+            .deleteMe()
+            .should.be.eventually.fulfilled
+            .should.notify(done);
+        });
+
+        it('current session can be retrieved', function(done) {
+            var sessionToken = corbelDriver.config.config.iamToken.accessToken;
+
+            corbelDriver.iam.user()
+            .getMySession()
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('data.userId', user.id);
+                expect(response).to.have.deep.property('data.token', sessionToken);
+            })
+            .should.notify(done);
+        });
+    });
+});
