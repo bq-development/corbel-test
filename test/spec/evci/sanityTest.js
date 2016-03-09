635afc88726685a8d702dc9214fd830c52ffa345
vipereybq <victor.perezrey@bq.com>
2016-03-09 11:24:51 +0100

    [Patch 7][SILKROAD-2464] SANITY production test
    Change-Id: I41a281ea8710a8855123b75d29885023235154e5

---
+++ test/spec/evci/sanityTest.js
@@ -0,0 +1,26 @@
+describe('In EVCI module', function() {
+
+    var corbelDriver;
+    
+    before(function() {
+        var prodCredentials = corbelTest.CONFIG.PRODUCTION_CREDENTIALS;
+        var prodIamUser = prodCredentials.DEFAULT_USER_IAM;
+        corbelDriver = corbelTest.getCustomDriver(prodIamUser);
+    });
+
+    describe('when send ClientLog', function() {
+        it('[SANITY] success with ACCEPTED (202)', function(done) {
+            corbelDriver.evci.event('log.ClientLog').publish({
+                client: {
+                    client: 'SilkRoad-Tests'
+                },
+                logs: [{
+                    level: 'INFO',
+                    timestamp: Date.now(),
+                    message: 'Production tests'
+                }]
+            })
+            .should.be.eventually.fulfilled.and.notify(done); 
+        });
+    });
+});
