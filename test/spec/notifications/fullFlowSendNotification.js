85e475a9aee920d6cec3541215ef7baecbfae2e9
Cristian del Cerro <cristian.delcerro@bq.com>
2016-03-09 10:43:01 +0100

    Added full flow notification test
    Change-Id: I0dd2ea0c9e8627a563362d1d7f2ee22faf54aecb

---
+++ test/spec/notifications/fullFlowSendNotification.js
@@ -0,0 +1,109 @@
+describe('In NOTIFICATIONS module', function() {
+
+    describe('when testing full flow: create domain, create notification template and send notification', function() {
+        var corbelRootDriver;
+        var testDriver;
+        var notificationId;
+        var notificationDomainData;
+        var notificationTemplateData;
+        var notificationData; 
+        var domainIdCreated;
+        var emailRecipient;
+        var emailAuthorization;
+
+        var MAX_RETRY = 28;
+        var RETRY_PERIOD = 3;
+
+
+        before(function(done) {
+            var clientScopes = ['notifications:admin']; 
+            var domainId = 'domain-fullFlowTest-' + Date.now();
+            domainIdCreated = 'silkroad-qa:' + domainId;
+            var clientName = 'client-crudDomainTest-' + Date.now();
+            corbelRootDriver = corbelTest.drivers['ROOT_CLIENT'].clone();
+            
+            notificationDomainData = {};
+            notificationDomainData['properties'] = {
+                title: 'title-' + domainIdCreated
+            };
+            notificationDomainData['templates'] = {};
+            notificationDomainData['templates'][domainIdCreated + ':tempKey'] = domainIdCreated + ':tempValue';
+
+            notificationTemplateData = {
+                id: 'tempValue',
+                type: 'email',
+                sender: 'fullflownotification@bq.com',
+                text: 'text',
+                title: '{{{title}}}'
+            };
+
+            corbelTest.common.iam.createDomainAndClient(corbelRootDriver, domainId, clientName, clientScopes)
+            .then(function(clientInfo) {
+                testDriver = corbelTest.getCustomDriver({
+                    'clientId': clientInfo.id,
+                    'clientSecret': clientInfo.key,
+                    'scopes': clientScopes.join(' ')
+                });
+
+
+                return corbelTest.common.mail.random.getRandomMail()
+                    .should.be.eventually.fulfilled;
+            })
+            .then(function(response){
+                emailRecipient = response.emailData.email_addr; // jshint ignore:line
+                emailAuthorization = response.cookies.PHPSESSID;
+
+                notificationData = {
+                    notificationId: 'tempKey',
+                    recipient: emailRecipient,
+                    properties: {
+                    }
+                };
+            })
+            .should.notify(done);
+        });
+
+        it('it works correctly', function(done) {
+            testDriver.notifications.domain().create(notificationDomainData)
+            .should.be.eventually.fulfilled
+            .then(function() {
+                return testDriver.notifications.template()
+                .create(notificationTemplateData)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                var corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
+
+                return testDriver.notifications.notification()
+                .send(notificationData)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function() {
+                return corbelTest.common.utils.retry(function() {
+                        return corbelTest.common.mail.random.checkMail(emailAuthorization)
+                            .then(function(response) {
+                                if (response.emailList.list.length === 0) {
+                                    return Promise.reject();
+                                } else {
+                                    return response;
+                                }
+                            });
+                    }, MAX_RETRY, RETRY_PERIOD)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(response) {
+                emailAuthorization = response.cookies.PHPSESSID;
+                var emailId = response.emailList.list[0].mail_id; //jshint ignore:line
+
+                return corbelTest.common.mail.random.getMail(emailAuthorization, emailId)
+                .should.be.eventually.fulfilled;
+            })
+            .then(function(mail) {
+                expect(mail).to.have.property('mail_subject', 'title-' + domainIdCreated);
+                expect(mail).to.have.property('mail_body', 'text');
+            })
+            .should.notify(done);
+        });
+            
+    });
+});
