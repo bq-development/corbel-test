df9caa4f50cab10ad07d76b756b362c5228babd2
Ismael Madirolas <ismael.madirolas@bq.com>
2016-03-21 13:15:18 +0100

    better webfs test using cookie
    Change-Id: Ifeb3aa51a822065d0bfb053322f439efb3be11ab

--- test/spec/webfs/getResourceTest.js
+++ test/spec/webfs/getResourceTest.js
@@ -1,27 +1,48 @@
 describe('In WEBFS module', function() {
     var corbelDriver;
 
-    before(function() {
+    beforeEach(function() {
         corbelDriver = corbelTest.drivers['ADMIN_USER'].clone();
-        corbelTest.common.utils.replaceUriForProxyUse(corbelDriver, 'webfs');
+        corbelTest.common.utils.replaceUriForProxyUse(corbelDriver);
     });
 
     it('a resource can be retrieved with the same content-type as especified through webfs', function(done) {
-        corbelDriver.webfs.webfs('index.html').get({Accept: 'text/xml'})
-        .should.be.eventually.fulfilled
-        .then(function(response){
-            expect(response).to.have.deep.property('headers.content-type').and.to.contain('text/xml');
-        })
-        .should.notify(done);
+        corbelDriver.webfs.webfs('index.html').get({
+                Accept: 'text/html'
+            })
+            .should.be.eventually.fulfilled
+            .then(function(response) {
+                expect(response).to.have.deep.property('headers.content-type').and.to.contain('text/html');
+            })
+            .should.notify(done);
+    });
+
+    it('a resource can be retrieved using the cookie', function(done) {
+        var corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
+        corbelTest.common.utils.replaceUriForProxyUse(corbelDriver);
+        corbelDriver.iam.token().create({
+                    claims: {
+                        'basic_auth.username': corbelDriver.config.get('username'),
+                        'basic_auth.password': corbelDriver.config.get('password')
+                    }
+                },
+                true).should.be.eventually.fulfilled
+            .then(function() {
+                return corbel.request.send({
+                        url: corbelDriver.config.getCurrentEndpoint('webfs') + 'index.html',
+                        withCredentials: true
+                    })
+                    .should.be.eventually.fulfilled;
+            }).should.notify(done);
     });
 
     it('an error 404 is returned if the resorce does not exist in webfs', function(done) {
         corbelDriver.webfs.webfs('non existent').get()
-        .should.be.eventually.rejected
-        .then(function(e){
-          expect(e).to.have.property('status', 404);
-          expect(e).to.have.deep.property('data.error', 'not_found');
-        })
-        .should.notify(done);
+            .should.be.eventually.rejected
+            .then(function(e) {
+                expect(e).to.have.property('status', 404);
+                expect(e).to.have.deep.property('data.error', 'not_found');
+            })
+            .should.notify(done);
     });
 });
