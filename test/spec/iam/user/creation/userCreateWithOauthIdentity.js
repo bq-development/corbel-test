2953ca1a412a3bc71385e2fd50bb652c3ff79514
David Garcia <barduk91@gmail.com>
2016-03-23 15:37:14 +0100

    Add test add user with oauth and log in
    Change-Id: I3be687d63c15ddca54ba1adb8497f69441a41d36

---
+++ test/spec/iam/user/creation/userCreateWithOauthIdentity.js
@@ -0,0 +1,98 @@
+describe('In IAM module', function() {
+
+    describe('while testing create user with oauth identity', function(){
+
+        var corbelDriver;
+        var userId;
+        var domainId = corbelTest.CONFIG.DOMAIN;
+        var domainDefaultScopes;
+        var random = Date.now();
+        var oauthIdentifier;
+        var oauthCommon;
+        var oauthUserTest;
+        var token;
+        var emailDomain = '@funkifake.com';
+
+        var userTest = {
+            'username': 'createUserOauthTest' + random,
+            'password': 'passwordTest',
+            'email': 'createUserOauthTest' + random + '@funkifake.com'
+        };
+
+        var user = {
+            'firstName': 'createUserIam' + random,
+            'email': 'createUserIam.iam.' + random + emailDomain,
+            'username': 'createUserIam.iam.' + random + emailDomain
+        };
+
+        before(function(done) {
+
+            oauthCommon = corbelTest.common.oauth;
+            oauthUserTest = oauthCommon.getOauthUserTestParams();
+            corbelDriver = corbelTest.drivers['ADMIN_USER'].clone();
+            corbelTest.drivers['ROOT_CLIENT'].clone()
+                .domain(domainId).iam.domain()
+                .get()
+                .then(function(domain){
+                    domainDefaultScopes = domain.data.defaultScopes;
+                })
+                .then(function(){
+                   return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams())
+                        .create(userTest)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function () {
+                    return corbelDriver.oauth
+                        .authorization(oauthCommon.getClientParamsCode())
+                        .login(userTest.username, userTest.password)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    return corbelDriver.oauth
+                        .token(oauthCommon.getClientParamsToken())
+                        .get(response.data.query.code)
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function (response) {
+                    token = response.data['access_token'];
+
+                    return corbelDriver.oauth
+                        .user(oauthCommon.getClientParams(), token)
+                        .get('me')
+                        .should.be.eventually.fulfilled;
+                })
+                .then(function(id){
+                    oauthIdentifier = id.data.id;
+                })
+                .should.notify(done);
+        });
+
+        it.only('Logged in using oauth identity', function(done){
+            corbelDriver.iam.users()
+                .create(user)       
+                .should.be.eventually.fulfilled
+                .then(function(id) {
+                    userId = id;
+
+                    return corbelDriver.iam.user(userId)
+                        .addIdentity({
+                             'identity': {
+                                'oauthService': 'silkroad',
+                                'oauthId': oauthIdentifier
+                            }
+                        });
+                })
+                .then(function(){
+                    var args = {
+                        oauthService: 'silkroad',
+                        username: userTest.username + random,
+                        password: userTest.password
+                    };
+                    return corbelTest.common.clients.loginUser(args)
+                            .should.be.eventually.fulfilled;
+                })
+                .should.notify(done);
+        });
+    });
+});
