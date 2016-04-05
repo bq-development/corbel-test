453a6d16e886162d5c9a3b55e75d8b9ad2c3ff84
Francisco Sanchez <francisco.sanchez@bq.com>
2016-04-05 09:31:02 +0200

    improve webfs test
    Change-Id: I1e48549f723562a7d0b3c0b16f1aa9293233c5a2

--- test/spec/webfs/getResourceTest.js
+++ test/spec/webfs/getResourceTest.js
@@ -2,11 +2,10 @@ describe('In WEBFS module', function() {
     var corbelDriver;
 
     beforeEach(function() {
-        corbelDriver = corbelTest.drivers['ADMIN_USER'].clone();
-        corbelTest.common.utils.replaceUriForProxyUse(corbelDriver);
+        corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
     });
 
-    it('a resource can be retrieved with the same content-type as especified through webfs', function(done) {
+    it('a resource can be retrieved with the same content-type as specified through webfs', function(done) {
         corbelDriver.webfs.webfs('index.html').get({
                 Accept: 'text/html'
             })
@@ -18,7 +17,6 @@ describe('In WEBFS module', function() {
     });
 
     it('a resource can be retrieved using the cookie', function(done) {
-        var corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
         corbelTest.common.utils.replaceUriForProxyUse(corbelDriver);
         corbelDriver.iam.token().create({
                     claims: {
