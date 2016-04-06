3383feef00cba3e354ca917a29207e23e52bd63f
Francisco Sanchez <francisco.sanchez@bq.com>
2016-04-06 13:18:30 +0200

    fix delete resource with acl
    Change-Id: I073156cea27d8dec3bae5544654995af53155955

--- test/spec/resources/acl/deletion/deleteResourceWithAclErrorTest.js
+++ test/spec/resources/acl/deletion/deleteResourceWithAclErrorTest.js
@@ -6,7 +6,7 @@ describe('In RESOURCES module', function() {
             var corbelDriver;
             var corbelAdminDriver;
             var corbelRootDriver;
-            var COLLECTION_NAME = 'test:testAcl' + Date.now();
+            var COLLECTION_NAME;
             var DOMAIN = 'silkroad-qa';
             var user;
             var adminUser;
@@ -17,179 +17,185 @@ describe('In RESOURCES module', function() {
             var TEST_OBJECT;
             var aclConfigurationId;
 
-            before(function(done) {
+            beforeEach(function(done) {
+                COLLECTION_NAME = 'test:testAcl' + Date.now();
                 corbelRootDriver = corbelTest.drivers['ADMIN_USER'].clone();
                 corbelDriver = corbelTest.drivers['DEFAULT_USER'].clone();
                 corbelAdminDriver = corbelTest.drivers['DEFAULT_USER'].clone();
                 usersId = [];
                 groupId = 'testGroup' + Date.now();
-                
-                corbelTest.common.resources.setManagedCollection(
-                    corbelRootDriver, DOMAIN, COLLECTION_NAME)
-                .should.be.eventually.fulfilled
-                .then(function(id){
-                    aclConfigurationId = id;
-                    return corbelTest.common.iam.createUsers(corbelAdminDriver, 1)
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function(createdUser) {
-                    adminUser = createdUser[0];
-                    usersId.push(adminUser.id);
 
-                    return corbelTest.common.iam.createUsers(corbelDriver, 1, {'groups': [groupId]})
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function(createdUser){
-                    user = createdUser[0];
-                    usersId.push(user.id);
-
-                    return corbelTest.common.clients.loginUser
-                        (corbelAdminDriver, adminUser.username, adminUser.password)
-                    .should.be.eventually.fulfilled;
-                })
-                .then(function(){
-                    return corbelTest.common.clients.loginUser
-                        (corbelDriver, user.username, user.password)
-                    .should.be.eventually.fulfilled;
-                })
-                .should.notify(done);
-            });
-
-            beforeEach(function(done) {
                 random = Date.now();
                 TEST_OBJECT = {
                     test: 'test' + random,
                     test2: 'test2' + random
                 };
 
-                corbelAdminDriver.resources.collection(COLLECTION_NAME)
-                    .add(TEST_OBJECT)
-                .should.be.eventually.fulfilled
-                .then(function(id) {
-                    resourceId = id;
-                })
-                .should.notify(done);
+                corbelTest.common.resources.setManagedCollection(
+                        corbelRootDriver, DOMAIN, COLLECTION_NAME)
+                    .should.be.eventually.fulfilled
+                    .then(function(id) {
+                        aclConfigurationId = id;
+                        return corbelTest.common.iam.createUsers(corbelAdminDriver, 1)
+                            .should.be.eventually.fulfilled;
+                    })
+                    .then(function(createdUser) {
+                        adminUser = createdUser[0];
+                        usersId.push(adminUser.id);
+
+                        return corbelTest.common.iam.createUsers(corbelDriver, 1, {
+                                'groups': [groupId]
+                            })
+                            .should.be.eventually.fulfilled;
+                    })
+                    .then(function(createdUser) {
+                        user = createdUser[0];
+                        usersId.push(user.id);
+
+                        return corbelTest.common.clients
+                            .loginUser(corbelAdminDriver, adminUser.username, adminUser.password)
+                            .should.be.eventually.fulfilled;
+                    })
+                    .then(function() {
+                        return corbelTest.common.clients.loginUser(corbelDriver, user.username, user.password)
+                            .should.be.eventually.fulfilled;
+                    })
+                    .then(function() {
+                        return corbelAdminDriver.resources.collection(COLLECTION_NAME)
+                            .add(TEST_OBJECT)
+                            .should.be.eventually.fulfilled;
+                    })
+                    .then(function(id) {
+                        resourceId = id;
+                    })
+                    .should.notify(done);
             });
+
 
             afterEach(function(done) {
-                
                 corbelAdminDriver.resources.resource(COLLECTION_NAME, resourceId)
                     .delete()
-                .should.be.eventually.fulfilled.and.notify(done);
-            });
+                    .should.be.eventually.fulfilled
+                    .then(function(id) {
+                        return corbelTest.common.resources.unsetManagedCollection(
+                                corbelRootDriver, DOMAIN, COLLECTION_NAME, aclConfigurationId)
+                            .should.be.eventually.fulfilled;
+                    })
+                    .then(function() {
+                        var promises = usersId.map(function(userId) {
+                            return corbelRootDriver.iam.user(userId)
+                                .delete()
+                                .should.be.eventually.fulfilled;
+                        });
 
-            after(function(done) {
-
-                corbelTest.common.resources.unsetManagedCollection(
-                    corbelRootDriver, DOMAIN, COLLECTION_NAME, aclConfigurationId)
-                .should.be.eventually.fulfilled
-                .then(function(){
-                    var promises = usersId.map(function(userId){
-                        return corbelRootDriver.iam.user(userId)
-                            .delete()
-                        .should.be.eventually.fulfilled;
-                    });
-
-                    return Promise.all(promises);
-                })
-                .should.notify(done);
+                        return Promise.all(promises);
+                    })
+                    .should.notify(done);
             });
 
             it('an error [401] is returned if the user only has WRITE permission', function(done) {
-               var ACL = {};
+                var ACL = {};
                 ACL['user:' + adminUser.id] = {
-                    permission : 'ADMIN'
+                    permission: 'ADMIN'
                 };
                 ACL['user:' + user.id] = {
-                    permission : 'WRITE'
+                    permission: 'WRITE'
                 };
 
                 corbelAdminDriver.resources.resource(COLLECTION_NAME, resourceId)
-                .update(ACL, {dataType: 'application/corbel.acl+json'})
-                .should.be.eventually.fulfilled
-                .then(function(){
+                    .update(ACL, {
+                        dataType: 'application/corbel.acl+json'
+                    })
+                    .should.be.eventually.fulfilled
+                    .then(function() {
 
-                    return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
-                        .delete()
-                    .should.be.eventually.rejected;
-                })
-                .then(function(e) {
-                    expect(e).to.have.property('status', 401);
-                    expect(e).to.have.deep.property('data.error', 'unauthorized');
-                })
-                .should.notify(done);
+                        return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                            .delete()
+                            .should.be.eventually.rejected;
+                    })
+                    .then(function(e) {
+                        expect(e).to.have.property('status', 401);
+                        expect(e).to.have.deep.property('data.error', 'unauthorized');
+                    })
+                    .should.notify(done);
             });
 
             it('an error [401] is returned if the users group only has WRITE permission', function(done) {
                 var ACL = {};
                 ACL['user:' + adminUser.id] = {
-                    permission : 'ADMIN'
+                    permission: 'ADMIN'
                 };
                 ACL['group:' + groupId] = {
-                    permission : 'WRITE'
+                    permission: 'WRITE'
                 };
 
                 corbelAdminDriver.resources.resource(COLLECTION_NAME, resourceId)
-                .update(ACL, {dataType: 'application/corbel.acl+json'})
-                .should.be.eventually.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
-                        .delete()
-                    .should.be.eventually.rejected;
-                })
-                .then(function(e) {
-                    expect(e).to.have.property('status', 401);
-                    expect(e).to.have.deep.property('data.error', 'unauthorized');
-                })
-                .should.notify(done);
+                    .update(ACL, {
+                        dataType: 'application/corbel.acl+json'
+                    })
+                    .should.be.eventually.fulfilled
+                    .then(function() {
+                        return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                            .delete()
+                            .should.be.eventually.rejected;
+                    })
+                    .then(function(e) {
+                        expect(e).to.have.property('status', 401);
+                        expect(e).to.have.deep.property('data.error', 'unauthorized');
+                    })
+                    .should.notify(done);
             });
 
             it('an error [401] is returned if the user only has READ permission', function(done) {
-               var ACL = {};
+                var ACL = {};
                 ACL['user:' + adminUser.id] = {
-                    permission : 'ADMIN'
+                    permission: 'ADMIN'
                 };
                 ACL['user:' + user.id] = {
-                    permission : 'READ'
+                    permission: 'READ'
                 };
 
                 corbelAdminDriver.resources.resource(COLLECTION_NAME, resourceId)
-                .update(ACL, {dataType: 'application/corbel.acl+json'})
-                .should.be.eventually.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
-                        .delete()
-                    .should.be.eventually.rejected;
-                })
-                .then(function(e) {
-                    expect(e).to.have.property('status', 401);
-                    expect(e).to.have.deep.property('data.error', 'unauthorized');
-                })
-                .should.notify(done);
+                    .update(ACL, {
+                        dataType: 'application/corbel.acl+json'
+                    })
+                    .should.be.eventually.fulfilled
+                    .then(function() {
+                        return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                            .delete()
+                            .should.be.eventually.rejected;
+                    })
+                    .then(function(e) {
+                        expect(e).to.have.property('status', 401);
+                        expect(e).to.have.deep.property('data.error', 'unauthorized');
+                    })
+                    .should.notify(done);
             });
 
             it('an error [401] is returned if the users group only has READ permission', function(done) {
                 var ACL = {};
                 ACL['user:' + adminUser.id] = {
-                    permission : 'ADMIN'
+                    permission: 'ADMIN'
                 };
                 ACL['group:' + groupId] = {
-                    permission : 'READ'
+                    permission: 'READ'
                 };
 
                 corbelAdminDriver.resources.resource(COLLECTION_NAME, resourceId)
-                .update(ACL, {dataType: 'application/corbel.acl+json'})
-                .should.be.eventually.fulfilled
-                .then(function() {
-                    return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
-                        .delete()
-                    .should.be.eventually.rejected;
-                })
-                .then(function(e) {
-                    expect(e).to.have.property('status', 401);
-                    expect(e).to.have.deep.property('data.error', 'unauthorized');
-                })
-                .should.notify(done);
+                    .update(ACL, {
+                        dataType: 'application/corbel.acl+json'
+                    })
+                    .should.be.eventually.fulfilled
+                    .then(function() {
+                        return corbelDriver.resources.resource(COLLECTION_NAME, resourceId)
+                            .delete()
+                            .should.be.eventually.rejected;
+                    })
+                    .then(function(e) {
+                        expect(e).to.have.property('status', 401);
+                        expect(e).to.have.deep.property('data.error', 'unauthorized');
+                    })
+                    .should.notify(done);
             });
         });
     });
