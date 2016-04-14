860c0d0c4e2c7a0bf20aa1fd4b23e6d4f5226a32
Francisco Sanchez <francisco.sanchez@bq.com>
2016-04-14 17:52:04 +0200

    improve survey test performance
    Change-Id: Ic6980b8bea0524066191e78e97752000a73efef6

--- test/spec/resources/survey/sendAndRetrieveSurveyTest.js
+++ test/spec/resources/survey/sendAndRetrieveSurveyTest.js
@@ -1,244 +1,148 @@
 describe('In RESOURCES module', function() {
-
     describe('In SURVEY module', function() {
-
         describe('questions can be added and then a response survey is created and sended', function() {
             var popEmail = corbelTest.common.mail.mailInterface.popEmail;
 
             var corbelDriver;
-            var questionTemplateId;
-            var email;
-            var emailAuthorization;
-            var orderIdentifier;
-            var clientUrl = 'bquiz-client.com.s3-website-eu-west-1.amazonaws.com';
-            var bquizDriverValues;
             var bquizCorbelDriver;
-            var satDriverValues;
             var satCorbelDriver;
 
-            var TEST_QUESTION = {
-                description: 'This is the {{test}}',
-                title: 'This is the {{test}}',
-                sku: 'abc123'
+            var questionTemplateId;
+            var clientUrl = 'bquiz-client.com.s3-website-eu-west-1.amazonaws.com';
+
+            var getBquizCorbelDriver = function() {
+                return bquizCorbelDriver;
+            };
+
+            var getSatCorbelDriver = function() {
+                return satCorbelDriver;
+            };
+
+            var tests = [{
+                name: 'bquiz in Spanish [mail]',
+                driver: getBquizCorbelDriver,
+                language: 'es-ES',
+                username: 'Batman',
+                expectedSubject: 'Valora bq.'
+            }, {
+                name: 'sat in Spanish [mail]',
+                driver: getSatCorbelDriver,
+                language: 'es-ES',
+                username: 'Batman',
+                expectedSubject: 'Queremos saber tu'
+            }, {
+                name: 'bquiz in French [mail]',
+                driver: getBquizCorbelDriver,
+                language: 'fr-FR',
+                username: 'Vincent',
+                expectedSubject: 'votre avis compte'
+            }, {
+                name: 'bquiz in Portuguese [mail]',
+                driver: getBquizCorbelDriver,
+                language: 'pt-PT',
+                username: 'Bruno',
+                expectedSubject: 'Avalie a bq'
+            }];
+
+            var sendSurveys = function() {
+                var promises = [];
+                tests.forEach(function(test) {
+                    var driver = test.driver().clone();
+                    var promise = corbelTest.common.mail.mailInterface.getRandomMail().
+                    then(function(email) {
+                            test.email = email;
+                            var survey = {
+                                email: email,
+                                delay: 'P0D',
+                                questionsRequest: [{
+                                    id: questionTemplateId,
+                                    metadata: {
+                                        'test': 'test question'
+                                    }
+                                }, {
+                                    id: questionTemplateId,
+                                    metadata: {
+                                        'test': 'test2 question'
+                                    }
+                                }],
+                                language: test.language,
+                                orderId: 'order-' + Date.now(),
+                                username: test.username
+                            };
+
+                            return driver.resources.collection('bquiz:Survey')
+                                .add(survey)
+                                .should.be.eventually.fulfilled;
+                        })
+                        .then(function() {
+                            return driver.resources.resource('bquiz:Contact', test.email)
+                                .get()
+                                .should.be.eventually.fulfilled;
+                        })
+                        .then(function(response) {
+                            expect(response).to.have.deep.property('data.contact', true);
+                            expect(response).to.have.deep.property('data.id', test.email);
+                        });
+
+                    promises.push(promise);
+                });
+                return Promise.all(promises);
             };
 
             before(function(done) {
-                bquizDriverValues = corbelTest.CONFIG.clientCredentials.bquiz.CLIENT;
+                var bquizDriverValues = corbelTest.CONFIG.clientCredentials.bquiz.CLIENT;
                 bquizDriverValues.domain = corbelTest.CONFIG.clientCredentials.bquiz.DOMAIN;
                 bquizCorbelDriver = corbelTest.getCustomDriver(bquizDriverValues);
 
-                satDriverValues = corbelTest.CONFIG.clientCredentials.sat.CLIENT;
+                var satDriverValues = corbelTest.CONFIG.clientCredentials.sat.CLIENT;
                 satDriverValues.domain = corbelTest.CONFIG.clientCredentials.sat.DOMAIN;
                 satCorbelDriver = corbelTest.getCustomDriver(satDriverValues);
 
                 corbelDriver = corbelTest.drivers['ADMIN_CLIENT'].clone();
 
+                var question = {
+                    description: 'This is the {{test}}',
+                    title: 'This is the {{test}}',
+                    sku: 'abc123'
+                };
+
                 corbelDriver.resources.collection('bquiz:Question')
-                    .add(TEST_QUESTION)
+                    .add(question)
                     .should.be.eventually.fulfilled
                     .then(function(id) {
                         questionTemplateId = id;
                     })
                     .then(function() {
-                        return corbelTest.common.mail.mailInterface.getRandomMail()
+                        return sendSurveys()
                             .should.be.eventually.fulfilled;
-                    })
-                    .then(function(response) {
-                        email = response;
                     })
                     .should.notify(done);
             });
 
-            beforeEach(function() {
-                orderIdentifier = 'order-' + Date.now();
+            tests.forEach(function(test) {
+                it(test.name, function(done) {
+                    popEmail(test.email)
+                        .should.be.eventually.fulfilled
+                        .then(function(mail) {
+                            expect(mail).to.have.deep.property('subject').and.to.contain(test.expectedSubject);
+                            expect(mail).to.have.deep.property('content').and.to.contain(clientUrl);
+                            return corbelDriver.resources.resource('bquiz:Contact', test.email)
+                                .delete()
+                                .should.be.eventually.fulfilled;
+                        })
+                        .then(function() {
+                            return corbelDriver.resources.resource('bquiz:Contact', test.email)
+                                .get()
+                                .should.be.eventually.rejected;
+                        })
+                        .then(function(e) {
+                            expect(e).to.have.property('status', 404);
+                            expect(e).to.have.deep.property('data.error', 'not_found');
+                        })
+                        .should.notify(done);
+                });
             });
 
-            afterEach(function(done) {
-
-                corbelDriver.resources.resource('bquiz:Contact', email)
-                    .delete()
-                    .should.be.eventually.fulfilled
-                    .then(function() {
-                        return corbelDriver.resources.resource('bquiz:Contact', email)
-                            .get()
-                            .should.be.eventually.rejected;
-                    })
-                    .then(function(e) {
-                        expect(e).to.have.property('status', 404);
-                        expect(e).to.have.deep.property('data.error', 'not_found');
-                    })
-                    .should.notify(done);
-            });
-
-            it('bquiz in Spanish [mail]', function(done) {
-                var TEST_SURVEY = {
-                    email: email,
-                    delay: 'P0D',
-                    questionsRequest: [{
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test question'
-                        }
-                    }, {
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test2 question'
-                        }
-                    }],
-                    language: 'es-ES',
-                    orderId: orderIdentifier,
-                    username: 'Batman'
-                };
-
-                bquizCorbelDriver.resources.collection('bquiz:Survey')
-                    .add(TEST_SURVEY)
-                    .should.be.eventually.fulfilled
-                    .then(function() {
-                        return bquizCorbelDriver.resources.resource('bquiz:Contact', email)
-                            .get()
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(response) {
-                        expect(response).to.have.deep.property('data.contact', true);
-                        expect(response).to.have.deep.property('data.id', email);
-
-                        return popEmail(email)
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(mail) {
-                        expect(mail).to.have.deep.property('subject').and.to.contain('Valora bq.');
-                        expect(mail).to.have.deep.property('content').and.to.contain(clientUrl);
-                    })
-                    .should.notify(done);
-            });
-
-            it('sat in Spanish [mail]', function(done) {
-                var TEST_SURVEY = {
-                    email: email,
-                    delay: 'P0D',
-                    questionsRequest: [{
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test question'
-                        }
-                    }, {
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test2 question'
-                        }
-                    }],
-                    language: 'es-ES',
-                    orderId: orderIdentifier,
-                    username: 'Batman'
-                };
-
-                satCorbelDriver.resources.collection('bquiz:Survey')
-                    .add(TEST_SURVEY)
-                    .should.be.eventually.fulfilled
-                    .then(function() {
-                        return satCorbelDriver.resources.resource('bquiz:Contact', email)
-                            .get()
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(response) {
-                        expect(response).to.have.deep.property('data.contact', true);
-                        expect(response).to.have.deep.property('data.id', email);
-
-                        return popEmail(email)
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(mail) {
-                        expect(mail).to.have.deep.property('subject').and.to.contain('Queremos saber tu');
-                        expect(mail).to.have.deep.property('content').and.to.contain(clientUrl);
-                    })
-                    .should.notify(done);
-            });
-
-            it('bquiz in French [mail]', function(done) {
-                var TEST_SURVEY = {
-                    email: email,
-                    delay: 'P0D',
-                    questionsRequest: [{
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test question'
-                        }
-                    }, {
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test2 question'
-                        }
-                    }],
-                    language: 'fr-FR',
-                    orderId: orderIdentifier,
-                    username: 'Vincent'
-                };
-
-                bquizCorbelDriver.resources.collection('bquiz:Survey')
-                    .add(TEST_SURVEY)
-                    .should.be.eventually.fulfilled
-                    .then(function() {
-                        return bquizCorbelDriver.resources.resource('bquiz:Contact', email)
-                            .get()
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(response) {
-                        expect(response).to.have.deep.property('data.contact', true);
-                        expect(response).to.have.deep.property('data.id', email);
-
-                        return popEmail(email)
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(mail) {
-                        expect(mail).to.have.deep.property('subject')
-                            .and.to.contain('votre avis compte');
-                        expect(mail).to.have.deep.property('content').and.to.contain(clientUrl);
-                    })
-                    .should.notify(done);
-            });
-
-            it('bquiz in Portuguese [mail]', function(done) {
-                var TEST_SURVEY = {
-                    email: email,
-                    delay: 'P0D',
-                    questionsRequest: [{
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test question'
-                        }
-                    }, {
-                        id: questionTemplateId,
-                        metadata: {
-                            'test': 'test2 question'
-                        }
-                    }],
-                    language: 'pt-PT',
-                    orderId: orderIdentifier,
-                    username: 'Bruno'
-                };
-
-                bquizCorbelDriver.resources.collection('bquiz:Survey')
-                    .add(TEST_SURVEY)
-                    .should.be.eventually.fulfilled
-                    .then(function() {
-                        return bquizCorbelDriver.resources.resource('bquiz:Contact', email)
-                            .get()
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(response) {
-                        expect(response).to.have.deep.property('data.contact', true);
-                        expect(response).to.have.deep.property('data.id', email);
-
-                        return popEmail(email)
-                            .should.be.eventually.fulfilled;
-                    })
-                    .then(function(mail) {
-                        expect(mail).to.have.deep.property('subject')
-                            .and.to.contain('Avalie a bq');
-                        expect(mail).to.have.deep.property('content').and.to.contain(clientUrl);
-                    })
-                    .should.notify(done);
-            });
         });
     });
 });
